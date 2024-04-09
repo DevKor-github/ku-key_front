6c1d0eafbdc85b6c4e507e35f38b29406031e8db
SeungminCha <dominico1225@gmail.com>
2024-04-09 20:18:11 +0900

    feat:: eslint, prettier 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,84 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['import', '@typescript-eslint', 'jsx-a11y', 'react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['.*'],
+        paths: [
+          {
+            name: 'react',
+            importNames: ['default'],
+            message: "import React from 'react' makes bundle size larger."
+          }
+        ]
+      }
     ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        allowSeparatedGroups: true
+      }
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index', 'object', 'type', 'unknown'],
+        pathGroups: [
+          {
+            pattern: 'next',
+            group: 'builtin',
+            position: 'before'
+          },
+          {
+            pattern: 'react',
+            group: 'builtin'
+          },
+          {
+            pattern: '@tanstack/**',
+            group: 'builtin',
+            position: 'before'
+          },
+          {
+            pattern: '@/libs/**',
+            group: 'unknown'
+          },
+          {
+            pattern: '@/core/**',
+            group: 'unknown'
+          },
+          {
+            pattern: '@/store/**',
+            group: 'unknown'
+          },
+          {
+            pattern: '**/*.css.ts',
+            group: 'unknown',
+            position: 'after'
+          }
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ],
+    'no-else-return': 'error',
+    'no-console': 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off'
   },
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 🍪 KU-KEY-NEXT 🔑
+# 🍪 KU-KEY-REACT 🔑
 고려대학교로 온 교환학생을 위한 서비스, KU-KEY의 웹페이지 레포지토리입니다
 
 ## React + TypeScript + Vite
