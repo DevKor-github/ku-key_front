6da30a298daeed1279e6c5db99f05021b9e3d851
halionaz <samhsj@naver.com>
2024-05-06 22:50:22 +0900

    feat: Footer 구현

---
+++ src/components/Footer.tsx
@@ -0,0 +1,114 @@
+import { css } from '@styled-stytem/css'
+import { Link } from 'react-router-dom'
+
+import instagramIcon from '@/assets/instagram.svg'
+import kakaoIcon from '@/assets/kakaocorp.svg'
+import mailIcon from '@/assets/mail.svg'
+import notionIcon from '@/assets/notion.svg'
+
+const supportApps = [
+  { name: 'kakao-talk', src: kakaoIcon },
+  { name: 'mail', src: mailIcon },
+  { name: 'notion', src: notionIcon },
+  { name: 'instagram', src: instagramIcon },
+]
+
+const supportAppIcon = css({
+  w: '25px',
+  h: '25px',
+  cursor: 'pointer',
+})
+
+const tabs = css({
+  color: 'black.1',
+  fontSize: '14.112px',
+  fontWeight: '500',
+})
+
+const Footer = () => {
+  return (
+    <div
+      className={css({
+        h: '290px',
+        borderTop: '1.211px solid {colors.lightGray.2}',
+        bg: 'white',
+        display: 'flex',
+        flexDir: 'row',
+        p: '0 152px',
+        alignItems: 'center',
+      })}
+    >
+      <div className={css({ flex: 1, display: 'flex', flexDir: 'column', gap: '16px' })}>
+        <div
+          className={css({
+            color: 'black.1',
+            // todo: 폰트 사이즈 규격화 필요
+            fontSize: '26px',
+            fontWeight: '800',
+          })}
+        >
+          KU-KEY
+        </div>
+        <div
+          className={css({
+            color: 'black.1',
+            fontSize: '14.112px',
+            fontWeight: '500',
+            cursor: 'pointer',
+          })}
+        >
+          문의
+        </div>
+        <div
+          className={css({
+            display: 'flex',
+            flexDir: 'row',
+            gap: '20px',
+          })}
+        >
+          {supportApps.map(app => {
+            return <img className={supportAppIcon} src={app.src} alt={app.name} />
+          })}
+        </div>
+        <div className={css({ w: '534px', h: '1px', bg: 'black.1' })} />
+        <div
+          className={css({
+            color: 'darkGray.1',
+            fontSize: '14.112px',
+            fontWeight: '500',
+          })}
+        >
+          Copyright ⓒ 2024 KU-KEY RUN. All Rights Reserved.
+        </div>
+      </div>
+      <div
+        className={css({
+          flex: 1,
+          display: 'flex',
+          flexDir: 'column',
+          flexWrap: 'wrap',
+          gap: '20.3px',
+          h: '100px',
+        })}
+      >
+        <Link to="/" className={tabs}>
+          MY PAGE
+        </Link>
+        <Link to="/announcement" className={tabs}>
+          ANNOUNCEMENT
+        </Link>
+        <Link to="/timetable" className={tabs}>
+          TIMETABLE
+        </Link>
+        <Link to="/community" className={tabs}>
+          COMMUNITY
+        </Link>
+        <Link to="/matching" className={tabs}>
+          1:1 MATCHING
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,6 +1,8 @@
+import { css } from '@styled-stytem/css'
 import { Helmet } from 'react-helmet-async'
 import { Outlet } from 'react-router-dom'
 
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 
 const MainLayout = () => {
@@ -10,9 +12,15 @@ const MainLayout = () => {
         <title>KU-Key</title>
       </Helmet>
       <Header />
-      <div>
+      <div
+        className={css({
+          // 임시 값
+          height: '300px',
+        })}
+      >
         <Outlet />
       </div>
+      <Footer />
     </div>
   )
 }
