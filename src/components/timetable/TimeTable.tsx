e7e93539ecfb4e1ad230060a7ffa597ded7bf9d4
halionaz <samhsj@naver.com>
2024-05-10 15:58:49 +0900

    refactor: Grid Layout 실제 시간표 등록되는 범위로 한정 - Header와 Sidebar는 Flex로 빼서, 실제 시간표 위에 강의를 얹을 때 계산하기 편하도록 함

--- src/components/timetable/TimeTable.tsx
+++ src/components/timetable/TimeTable.tsx
@@ -11,34 +11,43 @@ const TimeCell = cva({
     fontSize: 18,
     fontWeight: '500',
     wordWrap: 'break-word',
+    h: '108px',
+    borderRight: '1px solid {colors.darkGray.2}',
+    borderBottom: '1px solid {colors.darkGray.2}',
   },
   variants: {
-    variant: {
-      header: {
+    header: {
+      true: {
         h: '40px',
       },
-      normal: {
-        h: '108px',
+    },
+    sidebar: {
+      true: {
+        background: 'lightGray.2',
+        w: '82px',
+      },
+    },
+    end: {
+      leftEnd: {
+        borderBottomLeftRadius: 10,
+      },
+      rightEnd: {
+        borderBottomRightRadius: 10,
       },
     },
   },
 })
 
-const TimeCellArr: string[] = []
-for (let i = 0; i < 48; i++) {
-  if (i % 6 === 0) {
-    TimeCellArr.push(`${i / 6 + 9}:00`)
-  } else {
-    TimeCellArr.push('')
-  }
+const time = ['']
+for (let i = 9; i <= 16; i++) {
+  time.push(`${i}:00`)
 }
+const week = ['MON', 'TUS', 'WEN', 'THR', 'FRI']
 
 interface TimeTableProps {
   semester: string
   year: string
 }
-
-const week = ['', 'MON', 'TUS', 'WEN', 'THR', 'FRI']
 
 const TimeTable = (props: TimeTableProps) => {
   const { semester, year } = props
@@ -76,39 +85,73 @@ const TimeTable = (props: TimeTableProps) => {
       </div>
       <div
         className={css({
-          display: 'grid',
-          gridTemplateColumns: '82px repeat(5, 1fr)',
+          display: 'flex',
+          flexDir: 'row',
           borderLeft: '1px solid {colors.darkGray.2}',
           borderBottomLeftRadius: 10,
-          '& div': {
-            borderRight: '1px solid {colors.darkGray.2}',
-            borderBottom: '1px solid {colors.darkGray.2}',
-          },
-          '& div:nth-child(6n+1)': {
-            background: 'lightGray.2',
-          },
-          '& div:nth-child(49)': {
-            borderBottomLeftRadius: 10,
-          },
-          '& div:nth-child(54)': {
-            borderBottomRightRadius: 10,
-          },
         })}
       >
-        {week.map((days, ind) => {
-          return (
-            <div key={ind} className={css(TimeCell.raw({ variant: 'header' }))}>
-              {days}
-            </div>
-          )
-        })}
-        {TimeCellArr.map((val, ind) => {
-          return (
-            <div key={ind} className={css(TimeCell.raw({ variant: 'normal' }))}>
-              {val}
-            </div>
-          )
-        })}
+        <div
+          className={css({
+            display: 'flex',
+            flexDir: 'column',
+          })}
+        >
+          {time.map((val, ind) => {
+            return (
+              <div
+                className={css(
+                  TimeCell.raw({ sidebar: true, header: ind === 0, end: ind === 8 ? 'leftEnd' : undefined }),
+                )}
+              >
+                {val}
+              </div>
+            )
+          })}
+        </div>
+        <div
+          className={css({
+            display: 'flex',
+            flexDir: 'column',
+            flex: 1,
+          })}
+        >
+          <div
+            className={css({
+              display: 'flex',
+              flexDir: 'row',
+            })}
+          >
+            {week.map((days, ind) => {
+              return (
+                <div
+                  key={ind}
+                  className={css(
+                    { flex: 1 },
+                    TimeCell.raw({
+                      header: true,
+                    }),
+                  )}
+                >
+                  {days}
+                </div>
+              )
+            })}
+          </div>
+
+          <div
+            className={css({
+              display: 'grid',
+              gridTemplateColumns: 'repeat(5, 1fr)',
+            })}
+          >
+            {Array(40)
+              .fill(0)
+              .map((_, ind) => {
+                return <div key={ind} className={css(TimeCell.raw({ end: ind === 39 ? 'rightEnd' : undefined }))}></div>
+              })}
+          </div>
+        </div>
       </div>
     </div>
   )
