b64e0156c6e6649f586db982f701ffec04068d15
SeungminCha <dominico1225@gmail.com>
2024-05-14 10:42:35 +0900

    chore:: dev server yarn script 이름 변경
    - yarn dev:liveserver 에서 dev:testserver 로

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useRoutes } from 'react-router-dom'
 import routes from '@/router/router'
 
 function App() {
+  console.log(import.meta.env.VITE_API_SERVER)
   const router = useRoutes(routes)
   return router
 }
