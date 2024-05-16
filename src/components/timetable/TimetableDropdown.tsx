bc45f5bece1e3cea273bfec342254cc2dad8b5c6
halionaz <samhsj@naver.com>
2024-05-16 22:20:19 +0900

    feat::Timetable Page에 Drop-down 추가 - Timetable Page에서 Semester를 선택할 수 있는 Dropdown - radix-ui 이용

---
+++ src/components/timetable/TimetableDropdown.tsx
@@ -0,0 +1,76 @@
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import { css } from '@styled-stytem/css'
+import { ChevronDown } from 'lucide-react'
+
+import { Semester } from '@/pages/MyTimeTablePage'
+
+interface TimetableDropdownProps {
+  semesterList: Semester[]
+  curSemester: number
+  setCurSemester: React.Dispatch<React.SetStateAction<number>>
+}
+
+const TimetableDropdown = ({ semesterList, curSemester, setCurSemester }: TimetableDropdownProps) => {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button
+          className={css({
+            w: 65,
+            rounded: 10,
+            border: '1px {colors.lightGray.2} solid',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: 2.5,
+            outline: 0,
+            cursor: 'pointer',
+          })}
+        >
+          <div className={css({ color: 'darkGray.2', fontSize: 20, fontWeight: 700, wordWrap: 'break-word' })}>
+            {`${semesterList[curSemester].year} ${semesterList[curSemester].semester} semester`}
+          </div>
+          <ChevronDown className={css({ color: 'darkGray.2' })} />
+        </button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          sideOffset={6}
+          className={css({
+            w: 65,
+            border: '1px {colors.lightGray.1} solid',
+            bgColor: 'bg',
+            rounded: 10,
+          })}
+        >
+          {semesterList.map((semester, ind) => {
+            return (
+              <DropdownMenu.Item
+                key={ind}
+                className={css({
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  py: 3.5,
+                  color: 'darkGray.1',
+                  fontSize: 18,
+                  fontWeight: 500,
+                  wordWrap: 'break-word',
+                  rounded: 10,
+                  border: ind == curSemester ? '1px {colors.darkGray.2} solid' : 'none',
+                  outline: 0,
+                  cursor: 'pointer',
+                })}
+                onClick={() => {
+                  setCurSemester(ind)
+                }}
+              >{`${semester.year} ${semester.semester} semester`}</DropdownMenu.Item>
+            )
+          })}
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
+}
+
+export default TimetableDropdown
--- src/pages/MyTimeTablePage.tsx
+++ src/pages/MyTimeTablePage.tsx
@@ -1,23 +1,42 @@
 import { css } from '@styled-stytem/css'
+import { useState } from 'react'
 
 import TimeTable from '@/components/timetable/TimeTable'
+import TimetableDropdown from '@/components/timetable/TimetableDropdown'
 import { ToolbarBtn } from '@/pages/TimeTablePage'
 
+// todo: 자료형 개선
+export interface Semester {
+  year: string
+  semester: string
+}
+
+const dummySemesterList: Semester[] = [
+  { year: '2024', semester: 'Fall' },
+  { year: '2024', semester: 'Spring' },
+]
+
 const MyTimeTablePage = () => {
+  const [curSemester, setCurSemester] = useState(0)
   return (
-    <div className={css({ display: 'flex', flexDir: 'column' })}>
-      <div className={css({ display: 'flex', flexDir: 'column', px: 20 })}>
-        <div className={css({ display: 'flex', flexDir: 'row', justifyContent: 'space-between', my: 6 })}>
+    <div className={css({ display: 'flex', flexDir: 'column', px: 20 })}>
+      <div className={css({ display: 'flex', flexDir: 'row', justifyContent: 'space-between', my: 6 })}>
+        <div className={css({ display: 'flex', flexDir: 'row', gap: 2.5 })}>
           <div className={css({ color: 'black.2', fontSize: 32, fontWeight: '800', wordWrap: 'break-word' })}>
             My schedule
           </div>
-          <div className={css({ display: 'flex', flexDir: 'row', gap: 1.5 })}>
-            <div className={css(ToolbarBtn.raw({ back: 'white' }))}>Link</div>
-            <div className={css(ToolbarBtn.raw({ back: 'white' }))}>⤵️</div>
-          </div>
+          <TimetableDropdown
+            semesterList={dummySemesterList}
+            curSemester={curSemester}
+            setCurSemester={setCurSemester}
+          />
         </div>
-        <TimeTable semester={'Spring'} year={'2024'} />
+        <div className={css({ display: 'flex', flexDir: 'row', gap: 1.5 })}>
+          <div className={css(ToolbarBtn.raw({ back: 'white' }))}>Link</div>
+          <div className={css(ToolbarBtn.raw({ back: 'white' }))}>⤵️</div>
+        </div>
       </div>
+      <TimeTable semester={'Spring'} year={'2024'} />
     </div>
   )
 }
