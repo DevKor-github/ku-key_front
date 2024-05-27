1ab102acf4f4812c25cca3b0bcd3f69194b6fa6d
halionaz <samhsj@naver.com>
2024-05-27 22:42:33 +0900

    comment:: FriendTimetable 페이지 작업 보류

--- src/pages/TimetablePage/FriendTimetablePage.tsx
+++ src/pages/TimetablePage/FriendTimetablePage.tsx
@@ -1,24 +1,27 @@
 import { css } from '@styled-stytem/css'
 
-import Timetable from '@/components/timetable'
+// import Timetable from '@/components/timetable'
 import FriendsList from '@/components/timetable/FriendsList'
-import { ToolbarBtn } from '@/pages/TimetablePage'
+
+// import { ToolbarBtn } from '@/pages/TimetablePage'
+
+// todo: 친구 시간표는 조금 더 고민해보자..
 
 const FriendTimetablePage = () => {
   return (
     <>
-      <div className={css({ display: 'flex', flexDir: 'row', justifyContent: 'space-between', my: 6 })}>
+      <div className={css({ display: 'flex', flexDir: 'row', justifyContent: 'space-between', my: 11 })}>
         <div className={css({ color: 'black.2', fontSize: 32, fontWeight: '800', wordWrap: 'break-word' })}>
-          Friend List
+          Friend list
         </div>
-        <div className={css({ display: 'flex', flexDir: 'row', gap: 1.5 })}>
+        {/* <div className={css({ display: 'flex', flexDir: 'row', gap: 1.5 })}>
           <div className={css(ToolbarBtn.raw({ back: 'white' }))}>Link</div>
           <div className={css(ToolbarBtn.raw({ back: 'white' }))}>⤵️</div>
-        </div>
+        </div> */}
       </div>
       <div className={css({ display: 'flex', flexDir: 'row', gap: 5 })}>
         <FriendsList />
-        <Timetable semester={'Spring'} year={'2024'} timetableID={'dummy'} />
+        {/* <Timetable semester={'Spring'} year={'2024'} timetableID={'dummy'} /> */}
       </div>
     </>
   )
