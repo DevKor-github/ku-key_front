971883e10ef635a8c2843254616d5b967a20fb83
halionaz <samhsj@naver.com>
2024-06-20 23:40:01 +0900

    mod:: 시간표 상단바 수정 - Color 선택 버튼 삭제, Plus 버튼과 Ellipsis 버튼으로 대체 - 시간표 이름 수정 로직 삭제 (Ellipsis Modal 내로 변경 예정)

---
+++ src/components/timetable/MyTimetable/ColorSelector.tsx
@@ -0,0 +1,38 @@
+import { css } from '@styled-stytem/css'
+
+const ColorSelector = () => {
+  return (
+    <div className={css({ display: 'flex', flexDir: 'row', gap: 1.5, alignItems: 'center' })}>
+      <div className={css({ color: 'lightGray.1', fontSize: 18, fontWeight: 500 })}>Color</div>
+      {/* todo: 드롭다운....구현해야지 */}
+      <div
+        className={css({
+          h: '1.375rem',
+          w: '1.375rem',
+          bgColor: 'red.3',
+          rounded: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)',
+          cursor: 'pointer',
+          _hover: {
+            boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.5)',
+          },
+        })}
+      >
+        <div
+          className={css({
+            h: 3,
+            w: 3,
+            bgColor: 'white',
+            rounded: '50%',
+            boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)',
+          })}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ColorSelector
--- src/components/timetable/index.tsx
+++ src/components/timetable/index.tsx
@@ -1,26 +1,31 @@
-import { css } from '@styled-stytem/css'
-import { useEffect, useState } from 'react'
+import { css, cva } from '@styled-stytem/css'
+import { Ellipsis, Plus } from 'lucide-react'
 
-import { useUpdateTableName } from '@/api/hooks/timetable'
 import { TimetableInfo } from '@/api/types/timetable'
 import TimetableLayout from '@/components/timetable/TimetableLayout'
 
+const optBtn = cva({
+  base: {
+    cursor: 'pointer',
+    transition: 'background 0.256s',
+    rounded: 'full',
+    h: '30px',
+    w: '30px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    _hover: {
+      bgColor: 'lightGray.1',
+    },
+  },
+})
 interface TimeTableProps {
   timetable: TimetableInfo
 }
 
 const Timetable = ({ timetable }: TimeTableProps) => {
+
   const { timeTableId, tableName, year, semester } = timetable
-  const [timetableTitle, setTimetableTitle] = useState('')
-  const { mutate: updateTableName } = useUpdateTableName()
-
-  useEffect(() => {
-    setTimetableTitle(tableName)
-  }, [tableName])
-
-  const changeTimetableTitle = (title: string) => {
-    updateTableName({ timeTableId, tableName: title })
-  }
 
   return (
     <div className={css({ w: '100%' })}>
@@ -42,43 +47,18 @@ const Timetable = ({ timetable }: TimeTableProps) => {
           <div className={css({ color: 'darkGray.1', fontSize: 20, fontWeight: 700, wordWrap: 'break-word' })}>
             {`${year} ${semester} semester`}
           </div>
-          <input
-            className={css({ border: 'none', color: 'lightGray.1', fontWeight: 500, fontSize: 18, outline: 'none' })}
-            value={timetableTitle}
-            onChange={event => {
-              setTimetableTitle(event.target.value)
-            }}
-            onBlur={event => changeTimetableTitle(event.target.value)}
-          ></input>
-        </div>
-        <div className={css({ display: 'flex', flexDir: 'row', gap: 1.5, alignItems: 'center' })}>
-          <div className={css({ color: 'lightGray.1', fontSize: 18, fontWeight: 500 })}>Color</div>
-          {/* todo: 드롭다운....구현해야지 */}
           <div
-            className={css({
-              h: '1.375rem',
-              w: '1.375rem',
-              bgColor: 'red.3',
-              rounded: '50%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)',
-              cursor: 'pointer',
-              _hover: {
-                boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.5)',
-              },
-            })}
+            className={css({ border: 'none', color: 'lightGray.1', fontWeight: 500, fontSize: 18, outline: 'none' })}
           >
-            <div
-              className={css({
-                h: 3,
-                w: 3,
-                bgColor: 'white',
-                rounded: '50%',
-                boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)',
-              })}
-            />
+            {tableName}
+          </div>
+        </div>
+        <div className={css({ display: 'flex', flexDir: 'row', gap: 4, alignItems: 'center', color: 'darkGray.1' })}>
+          <div className={optBtn()}>
+            <Plus size={20} />
+          </div>
+          <div className={optBtn()}>
+            <Ellipsis size={20} />
           </div>
         </div>
       </div>
