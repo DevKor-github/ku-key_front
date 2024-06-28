fd04e610a379e0bd8d9cbb80f9ecb703d05cce33
halionaz <samhsj@naver.com>
2024-06-28 16:13:57 +0900

    design:: 친구 검색창 스타일 수정 - Name | < 이건 Placeholder 느낌이 아니라 검색어 예제였던 것임..clear

--- src/components/timetable/FriendTimetable/FriendsManage.tsx
+++ src/components/timetable/FriendTimetable/FriendsManage.tsx
@@ -25,39 +25,29 @@ const FriendsManage = () => {
       })}
     >
       <form
-        className={css({
-          display: 'flex',
-          alignItems: 'center',
-          bgColor: 'lightGray.2',
-          rounded: 10,
-          px: 5,
-          py: 3,
-          border: '1px solid {colors.darkGray.2}',
-          color: 'black.2',
-        })}
         onSubmit={e => {
           e.preventDefault()
           setSearchKeyword(inputKeyword)
           setInputKeyword('')
         }}
       >
-        <div
-          className={css({
-            display: 'flex',
-            alignItems: 'center',
-            pr: 1,
-            borderRight: 'solid {colors.black.2} 1px',
-            h: 5,
-            fontSize: 18,
-          })}
-        >
-          Name
-        </div>
         <Input
           onChange={e => {
             setInputKeyword(e.target.value)
           }}
-          className={css({ bgColor: 'transparent', border: 'none', outline: 'none', h: 5, fontSize: 18 })}
+          className={css({
+            display: 'flex',
+            alignItems: 'center',
+            bgColor: 'lightGray.2',
+            outline: 'none',
+            rounded: 10,
+            px: 5,
+            py: 3,
+            h: 13,
+            fontSize: 18,
+            border: '1px solid {colors.darkGray.2}',
+            color: 'black.2',
+          })}
           value={inputKeyword}
         />
       </form>
