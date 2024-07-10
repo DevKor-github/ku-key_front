ce5486382b895902dc96214847e79b4d4ed3d73d
halionaz <samhsj@naver.com>
2024-07-10 22:30:10 +0900

    design:: 시간표 색상 임의 지정 - 더 나은 테스트 UX를 위해 색상 임의 지정

--- src/lib/constants/timetableColors.ts
+++ src/lib/constants/timetableColors.ts
@@ -7,22 +7,22 @@ export const COLOR_INFO: { [key in ColorType]: { symbol: string; rand: string[] 
   },
   Blue: {
     symbol: '#79D6F3',
-    rand: ['#DDE8F6', '#C5D3E4', 'rgba(212, 59, 59, 0.5)', 'rgba(255, 69, 69, 0.5)'],
+    rand: ['#DDE8F6', '#C5D3E4', '#A2E0F4', '#B0DDEB'],
   },
   Gray: {
     symbol: '#D6D6D6',
-    rand: ['rgba(255, 0, 0, 0.5)', 'rgba(243, 121, 121, 0.5)', 'rgba(212, 59, 59, 0.5)', 'rgba(255, 69, 69, 0.5)'],
+    rand: ['#D3D3D3', '#CCCCCC', '#E9E9E9', '#D6D6D6'],
   },
   Green: {
     symbol: '#91F379',
-    rand: ['#EFF9CD', '#DBF2E9', 'rgba(212, 59, 59, 0.5)', 'rgba(255, 69, 69, 0.5)'],
+    rand: ['#EFF9CD', '#DBF2E9', '#C3F2B7', '#B5CEAF'],
   },
   Orange: {
     symbol: '#F3AC79',
-    rand: ['rgba(255, 0, 0, 0.5)', 'rgba(243, 121, 121, 0.5)', 'rgba(212, 59, 59, 0.5)', 'rgba(255, 69, 69, 0.5)'],
+    rand: ['#EDA776', '#EDA876', '#EFB58A', '#FFAA45'],
   },
   Purple: {
     symbol: '#E279F3',
-    rand: ['#BE9EE8', '#D3BEED', 'rgba(212, 59, 59, 0.5)', 'rgba(255, 69, 69, 0.5)'],
+    rand: ['#BE9EE8', '#D3BEED', '#CC8ED5', '#DFB0E7'],
   },
 }
