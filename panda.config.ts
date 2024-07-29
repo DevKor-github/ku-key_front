cb18eb7463d839ac60c129fb089d3be38c16af2c
SeungminCha <dominico1225@gmail.com>
2024-07-29 16:22:23 +0900

    fix: syntax error

--- panda.config.ts
+++ panda.config.ts
@@ -3,14 +3,13 @@ import { defineConfig } from '@pandacss/dev'
 import {
   buttonRecipe,
   chipRecipe,
-  clubTagRecipe
+  clubTagRecipe,
   inputRecipe,
   labelRecipe,
   modalCardRecipe,
   shadowRecipe,
 } from './src/lib/recipes/index'
 import { tokenToRem } from './src/lib/constants/tokenToRem'
-
 
 export default defineConfig({
   // Whether to use css reset
