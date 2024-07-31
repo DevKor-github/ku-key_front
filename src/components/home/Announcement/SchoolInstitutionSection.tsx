f49a4f70351408acdc9e4b4c4f3c60ceb36087b5
SeungminCha <dominico1225@gmail.com>
2024-07-31 22:08:50 +0900

    feat:: Institution 부분 디자인 변경사항 반영

--- src/components/home/Announcement/SchoolInstitutionSection.tsx
+++ src/components/home/Announcement/SchoolInstitutionSection.tsx
@@ -5,44 +5,33 @@ import SectionTitle from '@/components/home/Announcement/SectionTitle'
 import { BROADCASTS } from '@/lib/school-institute/broadcasts'
 import { ETC } from '@/lib/school-institute/etc'
 
-const InsitutionTitle = css({
-  display: 'inline-flex',
-  px: 2,
-  py: 4,
-  justifyContent: 'center',
-  alignItems: 'center',
-  color: 'red.1',
-  fontSize: 20,
-  fontWeight: 700,
-  mt: '10px',
-})
-
 const ProfileContainer = css({
   display: 'flex',
   flexWrap: 'wrap',
-  w: 480,
-  px: 2,
-  alignItems: 'flex-start',
-  alignContent: 'flex-start',
-  columnGap: 4,
-  maxH: 222,
-  mb: 2.5,
+  w: 377,
+  h: 'auto',
+  px: 1.5,
+  py: 1.5,
+  alignItems: 'center',
+  alignContent: 'center',
+  columnGap: 10,
+  rowGap: 3.5,
 })
 const SchoolInstitutionSection = () => {
   return (
     <section className={css({ display: 'flex', w: 480, flexDir: 'column', alignItems: 'flex-start' })}>
-      <SectionTitle title="School Institution" />
-      <h3 className={InsitutionTitle}>Broadcast</h3>
-      <div className={ProfileContainer}>
-        {BROADCASTS.map(broadcast => (
-          <InsituteProfile key={broadcast.name} name={broadcast.name} img={broadcast.img} url={broadcast.url} />
-        ))}
-      </div>
-      <h3 className={InsitutionTitle}>ETC</h3>
-      <div className={cx(ProfileContainer, css({ maxH: 118 }))}>
-        {ETC.map(etc => (
-          <InsituteProfile key={etc.name} name={etc.name} img={etc.img} url={etc.url} />
-        ))}
+      <SectionTitle title="Institution" description="Click the link to go to the official page" />
+      <div className={css({ display: 'flex', flexDir: 'column', gap: 15 })}>
+        <div className={ProfileContainer}>
+          {BROADCASTS.map(broadcast => (
+            <InsituteProfile key={broadcast.name} name={broadcast.name} img={broadcast.img} url={broadcast.url} />
+          ))}
+        </div>
+        <div className={ProfileContainer}>
+          {ETC.map(etc => (
+            <InsituteProfile key={etc.name} name={etc.name} img={etc.img} url={etc.url} />
+          ))}
+        </div>
       </div>
     </section>
   )
