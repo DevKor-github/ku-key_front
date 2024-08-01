e02106f0f61847f31200202faf955f1aec1143ee
SeungminCha <dominico1225@gmail.com>
2024-08-01 15:27:01 +0900

    feat:: 달력 구현 레이아웃 잡기

--- src/components/home/Announcement/CalendarSection.tsx
+++ src/components/home/Announcement/CalendarSection.tsx
@@ -6,9 +6,21 @@ import SectionTitle from '@/components/home/Announcement/SectionTitle'
 
 const CalendarSection = () => {
   return (
-    <section className={css({ display: 'flex', w: 500, flexDir: 'column' })}>
+    <section className={css({ display: 'flex', flexDir: 'column' })}>
       <SectionTitle title="Calendar" description="Check your academic schedule" link="/calendar" />
-      <div className={css({ display: 'flex', justifyContent: 'space-between' })}>
+      <div
+        className={css({
+          display: 'inline-flex',
+          alignItems: 'flex-start',
+          pt: '30px',
+          pl: 10,
+          pr: 5,
+          pb: 2.5,
+          bgColor: 'white',
+          rounded: 20,
+          boxShadow: '0px 0px 4px 0px rgba(0, 0, 0, 0.25)',
+        })}
+      >
         <CalendarHeader />
         <CalendarContainer />
       </div>
--- src/components/home/Announcement/index.tsx
+++ src/components/home/Announcement/index.tsx
@@ -10,10 +10,12 @@ const Announcement = () => {
         display: 'flex',
         flexDir: 'row',
         bgColor: 'bg.gray',
+        justifyContent: 'center',
         // py: '39px',
-        justifyContent: 'space-between',
-        px: 'calc((100vw - 1027px)/2)',
+        // justifyContent: 'space-between',
+        // px: 'calc((100vw - 1027px)/2)',
         flexWrap: 'wrap',
+        gap: 10,
       })}
     >
       <CalendarSection />
