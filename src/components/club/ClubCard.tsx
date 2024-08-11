06a0d7899e074a8e02ff541a01354fe571c0575d
halionaz <samhsj@naver.com>
2024-08-11 16:42:39 +0900

    feat:: Club Card 디자인

--- src/components/club/ClubCard.tsx
+++ src/components/club/ClubCard.tsx
@@ -1,10 +1,48 @@
+import { css } from '@styled-stytem/css'
+
 import { ClubInterface } from '@/types/club'
 
 interface ClubCardProps {
   clubData: ClubInterface
 }
 const ClubCard = ({ clubData }: ClubCardProps) => {
-  return <div>{clubData.name}</div>
+  return (
+    <div className={css({ display: 'flex', justifyContent: 'space-between', alignItems: 'center' })}>
+      <div className={css({ display: 'flex', gap: 5 })}>
+        <img
+          className={css({ w: '294px', h: '250px', objectFit: 'cover', rounded: 10, flexShrink: 0 })}
+          src="https://previews.123rf.com/images/avs1/avs12006/avs1200600713/149429617-%ED%88%AC%EB%AA%85-%EB%B0%B0%EA%B2%BD%EC%9E%85%EB%8B%88%EB%8B%A4-%ED%88%AC%EB%AA%85-%EA%B7%B8%EB%A6%AC%EB%93%9C-%EB%B2%A1%ED%84%B0-%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-%EB%A0%88%EC%9D%B4-%EC%85%98.jpg"
+          alt={clubData.name}
+        />
+        <div className={css({ display: 'flex', flexDir: 'column', gap: 6 })}>
+          <div className={css({ display: 'flex', flexDir: 'column', gap: 4 })}>
+            <div className={css({ display: 'flex', flexDir: 'column', gap: 1.5 })}>
+              <p className={css({ fontSize: 18 })}>{clubData.summary}</p>
+              <h2 className={css({ fontSize: 30, fontWeight: 700 })}>{clubData.name}</h2>
+            </div>
+            <div
+              className={css({
+                display: 'flex',
+                flexDir: 'column',
+                gap: 1.5,
+                fontSize: 16,
+                fontWeight: 600,
+                '& span': {
+                  color: 'red.1',
+                },
+              })}
+            >
+              <p>Regular Meeting | {clubData.regularMeeting}</p>
+              <p>Recruitment Period | {clubData.recruitmentPeriod}</p>
+            </div>
+          </div>
+          <div className={css({ display: 'flex', flexDir: 'column', gap: 3 })}>
+            <p className={css({ fontWeight: 400, color: 'darkGray.1', fontSize: 16 })}>{clubData.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ClubCard
--- src/components/club/SearchArea.tsx
+++ src/components/club/SearchArea.tsx
@@ -1,17 +1,96 @@
-import { useState } from 'react'
+import { css, cva } from '@styled-stytem/css'
+import { Search, X } from 'lucide-react'
+import { useCallback, useRef, useState } from 'react'
 
 interface SearchAreaProps {
   onSubmit: (inputKeyword: string) => void
 }
 const SearchArea = ({ onSubmit }: SearchAreaProps) => {
+  const inputRef = useRef<HTMLInputElement | null>(null)
   const [keyword, setKeyword] = useState('')
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    onSubmit(keyword)
-  }
+
+  const handleSubmit = useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+      onSubmit(keyword)
+    },
+    [keyword, onSubmit],
+  )
+
+  const handleClearSearchBox = useCallback(() => {
+    setKeyword('')
+    inputRef.current?.focus()
+  }, [])
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={keyword} onChange={e => setKeyword(e.target.value)} />
+    <form
+      onSubmit={handleSubmit}
+      className={css({
+        position: 'relative',
+        width: '608px',
+        h: 12,
+        display: 'flex',
+        alignItems: 'center',
+        borderColor: 'lightGray.1',
+        border: '1px solid',
+        rounded: 'full',
+        fontSize: 18,
+      })}
+    >
+      <span
+        className={cva({
+          base: {
+            position: 'absolute',
+            left: 5,
+            zIndex: 0,
+            visibility: 'hidden',
+            color: 'darkGray.2',
+            display: 'flex',
+            gap: 2.5,
+            alignItems: 'center',
+          },
+          variants: {
+            isVisible: {
+              true: {
+                visibility: 'visible',
+              },
+            },
+          },
+        })({ isVisible: keyword === '' })}
+      >
+        <Search size={24} />
+        search
+      </span>
+      <input
+        type="text"
+        ref={inputRef}
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
+        className={css({ zIndex: 1, pl: 5, w: 'full', h: 'full', outline: 'none', border: 'none' })}
+      />
+      <button
+        type="button"
+        onClick={handleClearSearchBox}
+        className={cva({
+          base: {
+            position: 'absolute',
+            right: 5,
+            zIndex: 2,
+            visibility: 'hidden',
+            color: 'black.1',
+            cursor: 'pointer',
+          },
+          variants: {
+            isVisible: {
+              true: {
+                visibility: 'visible',
+              },
+            },
+          },
+        })({ isVisible: keyword !== '' })}
+      >
+        <X />
+      </button>
     </form>
   )
 }
