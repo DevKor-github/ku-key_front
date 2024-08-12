501ccd4cf99215093bbb0d70ee29f82f009772b9
SeungminCha <dominico1225@gmail.com>
2024-08-12 17:10:25 +0900

    feat:: 대댓글 입력 창 구현

--- src/components/community/post/CommentInput.tsx
+++ src/components/community/post/CommentInput.tsx
@@ -1,71 +1,77 @@
 import { css } from '@styled-stytem/css'
-import { reactionButton } from '@styled-stytem/recipes'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useAtomValue } from 'jotai'
-import { SendHorizonal } from 'lucide-react'
+import { Forward, SendHorizonal } from 'lucide-react'
+import { useCallback, useState } from 'react'
 
+import { Checkbox } from '@/components/ui/checkbox'
 import { MemoizedTextAreaAutosize } from '@/components/ui/textarea-autosize'
 import { isInputOpenAtom } from '@/lib/store/comment'
+import { useTextArea } from '@/util/useTextArea'
 
 const CommentInput = () => {
   const isOpen = useAtomValue(isInputOpenAtom)
+  const { value, onChange } = useTextArea('')
+  const [anonymous, setAnonymous] = useState(false)
+  const handleAnonymous = useCallback(() => setAnonymous(prev => !prev), [])
+
   return (
     <AnimatePresence>
       {isOpen && (
-        <motion.div
+        <motion.label
           key="comment-input"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
+          htmlFor="comment"
           className={css({
             display: 'flex',
-            flexDir: 'column',
-            alignItems: 'flex-end',
-            gap: 2.5,
-            alignSelf: 'stretch',
+            px: 5,
+            h: 'auto',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+            w: 'full',
+            py: '13px',
+            alignSelf: 'flex-end',
+            rounded: 10,
+            border: '1px solid {colors.darkGray.2}',
+            resize: 'none',
+            gap: 4,
+            maxW: 756,
           })}
         >
-          <label
-            htmlFor="comment"
+          <span hidden>send</span>
+          <div>
+            <Forward className={css({ color: 'darkGray.2', transform: 'scale(1,-1)' })} size={24} />
+          </div>
+          <MemoizedTextAreaAutosize
+            value={value}
+            onChange={onChange}
+            maxRows={3}
+            form="comment"
+            placeholder="Add a comment..."
             className={css({
               display: 'flex',
-              px: 5,
-              h: 'auto',
-              justifyContent: 'space-between',
-              alignItems: 'center',
+              w: 'full',
               alignSelf: 'stretch',
-              rounded: 10,
-              border: '1px solid {colors.lightGray.1}',
-              bgColor: 'bg.gray',
-              _placeholder: { fontSize: 18, fontWeight: 500, color: 'lightGray.1' },
+              border: 'none',
               resize: 'none',
-              _focus: { outlineColor: 'darkGray.2' },
-              gap: 4,
+              outline: 'none',
+              textStyle: 'heading4_M',
+              color: 'darkGray.1',
+              _placeholder: { textStyle: 'heading4_M', color: 'lightGray.1' },
             })}
-          >
-            <span hidden>send</span>
-            <MemoizedTextAreaAutosize
-              maxRows={3}
-              form="comment"
-              placeholder="Add a comment..."
-              className={css({
-                display: 'flex',
-                w: 'full',
-                alignSelf: 'stretch',
-                border: 'none',
-                resize: 'none',
-                py: '11px',
-                outline: 'none',
-              })}
-            />
-            <button type="button" className={css({ display: 'flex', color: 'lightGray.1', cursor: 'pointer' })}>
-              <SendHorizonal />
+          />
+          <div className={css({ display: 'flex', gap: 4, alignItems: 'center' })}>
+            <div className={css({ display: 'flex', alignItems: 'center', gap: 1.5 })}>
+              <Checkbox checked={anonymous} onCheckedChange={handleAnonymous} />
+              <p className={css({ textStyle: 'heading4_M', color: 'darkGray.2' })}>Anonymous</p>
+            </div>
+            <button type="button" className={css({ display: 'flex', color: 'darkGray.2', cursor: 'pointer' })}>
+              <SendHorizonal style={{ color: value ? '#6B6B6B' : 'inherit' }} />
             </button>
-          </label>
-          <button aria-pressed={false} className={reactionButton()}>
-            Anonymous
-          </button>
-        </motion.div>
+          </div>
+        </motion.label>
       )}
     </AnimatePresence>
   )
