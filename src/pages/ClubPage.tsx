e4af1c3bfeeb32ddce25634f413df464deeca3bf
halionaz <samhsj@naver.com>
2024-08-21 12:11:02 +0900

    feat:: Wishlist Checkbox & No search Result - 좋아요한 동아리만 모아보기 - No Search Result 대응

--- src/pages/ClubPage.tsx
+++ src/pages/ClubPage.tsx
@@ -7,6 +7,7 @@ import CategorySelector from '@/components/club/CategorySelector'
 import ClubCard from '@/components/club/ClubCard'
 import { CategoryType } from '@/components/club/constants'
 import SearchArea from '@/components/club/SearchArea'
+import { Checkbox } from '@/components/ui/checkbox'
 import { useSearch } from '@/util/useSearch'
 
 const ClubPage = () => {
@@ -50,6 +51,10 @@ const ClubPage = () => {
     [likeClub, query],
   )
 
+  const handleWishList = useCallback(() => {
+    handleSetParam('wishlist', `${!query.wishList}`)
+  }, [handleSetParam, query.wishList])
+
   return (
     <>
       <div
@@ -72,11 +77,24 @@ const ClubPage = () => {
       <div className={css({ px: 64, pt: 29, display: 'flex', flexDir: 'column', gap: 19, bgColor: 'bg.gray' })}>
         <CategorySelector curCategory={query.category} setCategory={setCategory} />
         <div className={css({ display: 'flex', flexDir: 'column', gap: 20, pb: 30 })}>
-          <div className={css({ display: 'flex', justifyContent: 'center', alignItems: 'center' })}>
+          <div className={css({ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', gap: 4 })}>
             <SearchArea onSubmit={handleSubmit} clearKeywordParam={clearKeyword} />
+            <div className={css({ display: 'flex', alignItems: 'center', gap: 2.5, px: 2.5 })}>
+              <Checkbox checked={query.wishList} onCheckedChange={handleWishList} />
+              <p className={css({ textStyle: 'heading4_M', color: 'darkGray.2' })}>View only I like</p>
+            </div>
           </div>
-          <div className={css({ display: 'flex', flexDir: 'column', gap: 15 })}>
-            {data?.map(club => <ClubCard key={club.clubId} clubData={club} handleLikeClick={handleLikeClick} />)}
+          <div className={css({ display: 'flex', flexDir: 'column', gap: 11 })}>
+            {query.keyword && (
+              <div className={css({ fontSize: 30, fontWeight: 700, color: 'darkGray.1' })}>‘KU’ Search Results</div>
+            )}
+            <div className={css({ display: 'flex', flexDir: 'column', gap: 15 })}>
+              {data?.length ? (
+                data?.map(club => <ClubCard key={club.clubId} clubData={club} handleLikeClick={handleLikeClick} />)
+              ) : (
+                <div>No search results</div>
+              )}
+            </div>
           </div>
         </div>
       </div>
