90dd771cfaf77669a7e465500f7093744405b733
halionaz <samhsj@naver.com>
2024-08-24 10:34:23 +0900

    style:: 불필요한 주석 삭제

--- src/pages/TimetablePage/index.tsx
+++ src/pages/TimetablePage/index.tsx
@@ -1,41 +1,7 @@
 import { css } from '@styled-stytem/css'
 import { Outlet } from 'react-router-dom'
 
-// const TimetableBtn = cva({
-//   base: {
-//     h: 12,
-//     px: 7,
-//     rounded: 10,
-//     display: 'inline-flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     border: '1px white solid',
-//     cursor: 'pointer',
-//     color: 'white',
-//     fontSize: 18,
-//     fontWeight: 500,
-//     zIndex: 2,
-//     transition: 'background 0.256s',
-//   },
-//   variants: {
-//     selected: {
-//       true: {
-//         bgColor: 'white',
-//         color: 'red.2',
-//       },
-//       false: {
-//         _hover: {
-//           bgColor: '#FFFFFF33',
-//         },
-//       },
-//     },
-//   },
-// })
-
 const TimetablePage = () => {
-  // const curPath = useLocation().pathname
-  // const curPathRoot = curPath.split('/')[2]
-
   return (
     <>
       <div
