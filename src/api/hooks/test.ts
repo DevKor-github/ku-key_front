7f59470d1186e1161195967ba47b87f42b725ec2
SeungminCha <dominico1225@gmail.com>
2024-08-24 10:14:53 +0900

    feat:: mypage 배경 asset 추가

--- src/api/hooks/test.ts
+++
@@ -1,6 +0,0 @@
-import { apiInterface } from '@/util/axios/custom-axios'
-
-export const getTest = async () => {
-  const response = await apiInterface.get('/timetable/user')
-  return response.data
-}
