0ea7003d6e69a039bacea039ef75090562e1ca6e
halionaz <samhsj@naver.com>
2024-08-30 13:23:32 +0900

    fix: API isLogin 인자 추가

--- src/api/hooks/club.ts
+++ src/api/hooks/club.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 import { GetClubRequest, GetClubResponse, PostClubLikeRequest, PostClubLikeResponse } from '@/api/types/club'
+import { useAuth } from '@/util/auth/useAuth'
 import { apiInterface } from '@/util/axios/custom-axios'
 
 const getClub = async (params: GetClubRequest) => {
@@ -16,13 +17,14 @@ const getClub = async (params: GetClubRequest) => {
 }
 
 export const useGetClubSearch = (params: GetClubRequest) => {
+  const isLogin = useAuth().authState ?? false
   const query: GetClubRequest = {
     ...params,
     keyword: params.keyword || null,
   }
   return useQuery({
-    queryKey: ['clubSearchResult', query],
-    queryFn: () => getClub(query),
+    queryKey: ['clubSearchResult', query, isLogin],
+    queryFn: () => getClub({ ...query, isLogin }),
     retry: false,
   })
 }
@@ -34,11 +36,12 @@ const postClubLike = async ({ clubId }: PostClubLikeRequest) => {
 
 export const usePostClubLike = () => {
   const queryClient = useQueryClient()
+  const isLogin = useAuth().authState ?? false
   return useMutation({
     mutationFn: postClubLike,
     onSuccess: (response, { queryParams }) => {
       queryClient.setQueryData<GetClubResponse>(
-        ['clubSearchResult', { ...queryParams, keyword: queryParams.keyword || null }],
+        ['clubSearchResult', { ...queryParams, keyword: queryParams.keyword || null, isLogin }],
         oldData => {
           if (oldData !== undefined) {
             return oldData.map(club => (club.clubId === response.clubId ? response : club))
