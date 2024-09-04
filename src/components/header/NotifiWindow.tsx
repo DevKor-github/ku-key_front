65799f6780b881ffe623fc41aeca1f02e1fe1139
halionaz <samhsj@naver.com>
2024-09-04 14:16:38 +0900

    design:: Figma 디자인 적용

--- src/components/header/NotifiWindow.tsx
+++ src/components/header/NotifiWindow.tsx
@@ -1,5 +1,6 @@
 import * as Popover from '@radix-ui/react-popover'
-import { css } from '@styled-stytem/css'
+import { css, cx } from '@styled-stytem/css'
+import { shadow } from '@styled-stytem/recipes'
 import { Bell } from 'lucide-react'
 
 import AttendanceBtn from '@/components/header/notification/AttendanceBtn'
@@ -7,18 +8,28 @@ import AttendanceBtn from '@/components/header/notification/AttendanceBtn'
 const NotifiWindow = () => {
   return (
     <Popover.Root>
-      <Popover.Trigger>
+      <Popover.Trigger asChild>
         <button className={css({ display: 'flex', alignItems: 'center', justifyContent: 'center', cursor: 'pointer' })}>
           <Bell size={20} />
         </button>
       </Popover.Trigger>
       <Popover.Portal>
-        <Popover.Content sideOffset={30} className={css({ bgColor: 'darkGray.1', rounded: 10, w: '320px', p: 5 })}>
+        <Popover.Content
+          sideOffset={30}
+          align="end"
+          className={cx(css({ bgColor: 'darkGray.1', rounded: 10, w: '320px', p: 5 }), shadow())}
+        >
           <div className={css({ display: 'flex', flexDir: 'column', gap: 5 })}>
             <div className={css({ display: 'flex', alignItems: 'center', justifyContent: 'space-between' })}>
               <AttendanceBtn />
               <h2
-                className={css({ color: 'white', textAlign: 'center', fontSize: 24, fontWeight: 700, lineHeight: 1 })}
+                className={css({
+                  color: 'white',
+                  textAlign: 'center',
+                  fontSize: 24,
+                  fontWeight: 700,
+                  lineHeight: 1,
+                })}
               >
                 Alarm
               </h2>
--- src/components/header/notification/AttendanceBtn.tsx
+++ src/components/header/notification/AttendanceBtn.tsx
@@ -4,6 +4,35 @@ import { useCallback } from 'react'
 
 import { useGetAttendance, usePostAttendance } from '@/api/hooks/attendance'
 
+const ButtonStyle = cva({
+  base: {
+    display: 'flex',
+    bgColor: 'bg.gray',
+    border: '1px solid {colors.darkGray.2}',
+    rounded: 'full',
+    lineHeight: 1,
+    fontSize: 12,
+    fontWeight: 700,
+    color: 'darkGray.2',
+    px: 2.5,
+    py: 1,
+    alignItems: 'center',
+    gap: 1,
+    cursor: 'pointer',
+    transition: 'all 0.2s ease',
+  },
+  variants: {
+    isDone: {
+      true: {
+        color: 'red.1',
+        borderColor: 'red.1',
+        bgColor: 'bg.red.1',
+        cursor: 'default',
+      },
+    },
+  },
+})
+
 const AttendanceBtn = () => {
   const { data: isDoneTodayAttend } = useGetAttendance()
   const { mutate: attend } = usePostAttendance()
@@ -19,37 +48,7 @@ const AttendanceBtn = () => {
   }, [attend, isDoneTodayAttend])
 
   return (
-    <button
-      className={cva({
-        base: {
-          display: 'flex',
-          bgColor: 'bg.gray',
-          border: '1px solid {colors.darkGray.2}',
-          rounded: 'full',
-          lineHeight: 1,
-          fontSize: 12,
-          fontWeight: 700,
-          color: 'darkGray.2',
-          px: 2.5,
-          py: 1,
-          alignItems: 'center',
-          gap: 1,
-          cursor: 'pointer',
-          transition: 'all 0.2s ease',
-        },
-        variants: {
-          isDone: {
-            true: {
-              color: 'red.1',
-              borderColor: 'red.1',
-              bgColor: 'bg.red.1',
-              cursor: 'default',
-            },
-          },
-        },
-      })({ isDone: isDoneTodayAttend })}
-      onClick={handleClick}
-    >
+    <button className={ButtonStyle({ isDone: isDoneTodayAttend })} onClick={handleClick}>
       <Check size={16} strokeWidth={3.5} />
       <p>Check attendance</p>
     </button>
