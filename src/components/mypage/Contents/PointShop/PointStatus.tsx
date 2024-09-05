ce596584bafde0db66a5814c3466ef1531638a16
halionaz <samhsj@naver.com>
2024-09-05 15:26:52 +0900

    feat: Sugar Modal

--- src/components/mypage/Contents/PointShop/PointStatus.tsx
+++ src/components/mypage/Contents/PointShop/PointStatus.tsx
@@ -1,44 +1,5 @@
 import { css } from '@styled-stytem/css'
 
-const HowToBox = css({
-  flexGrow: 1,
-  bgColor: 'white',
-  h: '180px',
-  display: 'flex',
-  flexDir: 'column',
-  gap: 5,
-  px: 10,
-  justifyContent: 'center',
-})
-const HowToTitle = css({
-  fontSize: 24,
-  fontWeight: 500,
-})
-const HowToContent = css({
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  '& > span': {
-    color: 'red.1',
-    fontSize: 14,
-    fontWeight: 500,
-  },
-  '& > span:first-child': {
-    fontSize: 16,
-    fontWeight: 400,
-    color: 'darkGray.1',
-  },
-})
-const CostInterval = css({
-  display: 'flex',
-  alignItems: 'center',
-  gap: 2,
-  '& > span': {
-    color: 'darkGray.2',
-    fontSize: 14,
-  },
-})
-
 interface PointStatusProps {
   name: string
   point: number
@@ -63,46 +24,6 @@ const PointStatus = ({ name, point }: PointStatusProps) => {
           <span>|</span>
           <span className={css({ fontWeight: 600 })}>{point}</span>
         </span>
-      </div>
-      <div className={css({ display: 'flex', gap: 5 })}>
-        <div className={HowToBox}>
-          <h3 className={HowToTitle}>How to get</h3>
-          <div className={css({ display: 'flex', flexDir: 'column', gap: 2 })}>
-            <div className={HowToContent}>
-              <span>Attendance</span>
-              <span>30</span>
-            </div>
-            <div className={HowToContent}>
-              <span>Writing lecture review</span>
-              <span>100</span>
-            </div>
-            <div className={HowToContent}>
-              <span>Hot board</span>
-              <span>100</span>
-            </div>
-          </div>
-        </div>
-        <div className={HowToBox}>
-          <h3 className={HowToTitle}>How to use</h3>
-          <div className={css({ display: 'flex', flexDir: 'column', gap: 2 })}>
-            <div className={HowToContent}>
-              <span>Reading lecture reviews</span>
-              <span className={CostInterval}>
-                100 <span>-</span> 30
-              </span>
-            </div>
-            <div className={HowToContent}>
-              <span>Evolving characters</span>
-              <span className={CostInterval}>
-                400 <span>-</span> 30
-              </span>
-            </div>
-            <div className={HowToContent}>
-              <span>Changing characters</span>
-              <span>200</span>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   )
--- src/components/ui/modal/SugarModal.tsx
+++ src/components/ui/modal/SugarModal.tsx
@@ -1,9 +1,53 @@
 import { css } from '@styled-stytem/css'
 import { CircleAlert } from 'lucide-react'
 
+import Sugar from '@/assets/Sugar_sm.png'
 import Button from '@/components/ui/button'
 import ModalCard from '@/components/ui/modal'
 import ModalPortal, { ModalPortalProps } from '@/components/ui/modal/ModalPortal'
+
+const HowToBox = css({
+  flexGrow: 1,
+  bgColor: 'white',
+  h: '180px',
+  display: 'flex',
+  flexDir: 'column',
+  gap: 5,
+  px: 10,
+  py: 4,
+  justifyContent: 'center',
+})
+const HowToTitle = css({
+  fontSize: 24,
+  fontWeight: 500,
+  lineHeight: 1,
+})
+const HowToContent = css({
+  display: 'flex',
+  w: '320px',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  lineHeight: 1.2,
+  '& > span': {
+    color: 'black',
+    fontSize: 14,
+    fontWeight: 500,
+  },
+  '& > span:first-child': {
+    fontSize: 16,
+    fontWeight: 400,
+    color: 'darkGray.1',
+  },
+})
+const CostInterval = css({
+  display: 'flex',
+  alignItems: 'center',
+  gap: 2,
+  '& > span': {
+    color: 'darkGray.2',
+    fontSize: 14,
+  },
+})
 
 interface SugarModalProps extends Omit<ModalPortalProps, 'children'> {
   modalRef: React.RefObject<HTMLDivElement>
@@ -30,6 +74,74 @@ const SugarModal = (props: SugarModalProps) => {
             Sugar is a point unit used in KU-key.
             <br />
             You can get sugar by following the activities shown below.
+          </div>
+        </div>
+        <div className={css({ display: 'flex', gap: 2.5 })}>
+          <div className={HowToBox}>
+            <h3 className={HowToTitle}>How to get</h3>
+            <div className={css({ display: 'flex', flexDir: 'column', gap: 2 })}>
+              <div className={HowToContent}>
+                <span>Attendance</span>
+                <span>
+                  <img src={Sugar} alt="sugar" />
+                  30
+                </span>
+              </div>
+              <div className={HowToContent}>
+                <span>Writing lecture review</span>
+                <span>
+                  <img src={Sugar} alt="sugar" />
+                  100
+                </span>
+              </div>
+              <div className={HowToContent}>
+                <span>Hot board</span>
+                <span>
+                  <img src={Sugar} alt="sugar" />
+                  100
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className={HowToBox}>
+            <h3 className={HowToTitle}>How to use</h3>
+            <div className={css({ display: 'flex', flexDir: 'column', gap: 2 })}>
+              <div className={HowToContent}>
+                <span>Reading lecture reviews</span>
+                <span className={CostInterval}>
+                  <div>
+                    <img src={Sugar} alt="sugar" />
+                    100
+                  </div>
+                  <span>-</span>
+                  <div>
+                    <img src={Sugar} alt="sugar" />
+                    30
+                  </div>
+                </span>
+              </div>
+              <div className={HowToContent}>
+                <span>Evolving characters</span>
+                <span className={CostInterval}>
+                  <div>
+                    <img src={Sugar} alt="sugar" />
+                    400
+                  </div>
+                  <span>-</span>
+                  <div>
+                    <img src={Sugar} alt="sugar" />
+                    30
+                  </div>
+                </span>
+              </div>
+              <div className={HowToContent}>
+                <span>Changing characters</span>
+                <span>
+                  <img src={Sugar} alt="sugar" />
+                  200
+                </span>
+              </div>
+            </div>
           </div>
         </div>
         <div className={css({ display: 'flex', gap: 5 })}>
