4acc79ef6da3f3726c32aace4e65652a70aa37c5
halionaz <samhsj@naver.com>
2024-09-08 18:07:57 +0900

    fix: 출석체크 시 포인트 변동

--- src/api/hooks/attendance.ts
+++ src/api/hooks/attendance.ts
@@ -14,6 +14,7 @@ export const usePostAttendance = () => {
     mutationFn: postAttendance,
     onSuccess: () => {
       queryClient.setQueryData<boolean>(['attendance'], true)
+      queryClient.invalidateQueries({ queryKey: ['myProfile'] })
     },
   })
 }
