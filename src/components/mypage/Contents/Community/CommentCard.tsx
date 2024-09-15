8cca9995a565bb8094a6152f4886198e7f4a4d0b
halionaz <samhsj@naver.com>
2024-09-15 18:37:09 +0900

    mod:: My Community 댓글 카드 디자인 수정

--- src/components/mypage/Contents/Community/CommentCard.tsx
+++ src/components/mypage/Contents/Community/CommentCard.tsx
@@ -1,75 +1,68 @@
-import { css } from '@styled-system/css'
-import { boardTag, reactionButton } from '@styled-system/recipes'
-import { Cookie, MessageCircle } from 'lucide-react'
-import { useCallback } from 'react'
+import { css, cx } from '@styled-system/css'
+import { shadow } from '@styled-system/recipes'
+import { Link } from 'react-router-dom'
 
-import UtilButton from '@/components/community/post/UtilButton'
-import NoticeModal from '@/components/ui/modal/NoticeModal'
-import { POST_MESSAGES } from '@/lib/messages/community'
 import { MyCommentProps } from '@/types/community'
 import { getFormattedTimeString } from '@/util/getFormattedTimeString'
-import { useModal } from '@/util/useModal'
 
 interface CommentCardProps {
   comment: MyCommentProps
 }
 const CommentCard = ({ comment }: CommentCardProps) => {
-  const { isOpen: modalOpen, handleOpen } = useModal(true)
-
-  const handleNavigate = useCallback(() => {
-    window.open(`/community/community/post/${comment.postId}`, '_blank')
-  }, [comment.postId])
-
   return (
-    <div
-      className={css({
-        display: 'flex',
-        flexDir: 'column',
-        alignItems: 'flex-end',
-        gap: 2.5,
-        alignSelf: 'stretch',
-      })}
-    >
+    <Link to={`/community/community/post/${comment.postId}`}>
       <div
         className={css({
           display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
+          flexDir: 'column',
+          alignItems: 'flex-end',
+          gap: '18px',
           alignSelf: 'stretch',
         })}
       >
-        <div className={css({ display: 'flex', alignItems: 'center', gap: 2.5 })}>
-          <div className={boardTag({ variant: 'red' })}>{comment.isAnonymous ? 'Anonymous' : 'Author'}</div>
+        <div
+          className={css({
+            display: 'flex',
+            alignItems: 'center',
+            alignSelf: 'stretch',
+            gap: 2.5,
+          })}
+        >
+          <div
+            className={cx(
+              css({
+                px: 2.5,
+                py: 1,
+                fontSize: 12,
+                fontWeight: 700,
+                lineHeight: 1.2,
+                color: 'darkGray.2',
+                rounded: 'full',
+                bgColor: 'white',
+              }),
+              shadow(),
+            )}
+          >
+            {comment.isAnonymous ? 'Anonymous' : 'Me'}
+          </div>
           <p className={css({ fontSize: 18, fontWeight: 500, color: 'darkGray.2' })}>
             {getFormattedTimeString(comment.createdAt)}
           </p>
         </div>
-        <UtilButton isComment isMine={true} isEditable={false} forMyPage handleNavigation={handleNavigate} />
+        <p
+          className={css({
+            display: 'flex',
+            alignSelf: 'stretch',
+            whiteSpace: 'pre-wrap',
+            textStyle: 'heading4_M',
+            color: 'darkGray.1',
+            smDown: { fontSize: 14 },
+          })}
+        >
+          {comment.content}
+        </p>
       </div>
-      <p
-        className={css({
-          display: 'flex',
-          alignSelf: 'stretch',
-          whiteSpace: 'pre-wrap',
-          textStyle: 'heading4_M',
-          color: 'darkGray.1',
-          smDown: { fontSize: 14 },
-        })}
-      >
-        {comment.content}
-      </p>
-      <div className={css({ display: 'flex', alignItems: 'center', gap: 2.5 })}>
-        <button className={reactionButton()} onClick={handleNavigate}>
-          <MessageCircle size={22} />
-          <p>{comment.replyCount}</p>
-        </button>
-        <button aria-pressed={false} className={reactionButton()} onClick={handleOpen}>
-          <Cookie size={22} />
-          <p>{comment.likeCount}</p>
-        </button>
-      </div>
-      <NoticeModal content={POST_MESSAGES.NO_LIKE_OWN_COMMENT} isOpen={modalOpen} />
-    </div>
+    </Link>
   )
 }
 
