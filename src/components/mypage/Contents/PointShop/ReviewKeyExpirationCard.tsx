82cfb72ce84fea92967aa423bb8ff8f8cf075851
halionaz <samhsj@naver.com>
2024-09-15 11:05:06 +0900

    design:: Figma Style과 동기화 & 반응형 적용

--- src/components/mypage/Contents/PointShop/ReviewKeyExpirationCard.tsx
+++ src/components/mypage/Contents/PointShop/ReviewKeyExpirationCard.tsx
@@ -13,9 +13,6 @@ const ReviewKeyExpirationCard = () => {
   const today = new Date()
   const expireDate = new Date(data.date)
 
-  console.log(today)
-  console.log(expireDate)
-
   const remain = Math.abs(expireDate.getTime() - today.getTime())
   const days = Math.floor(remain / (1000 * 60 * 60 * 24))
   const hours = Math.floor((remain / (1000 * 60 * 60)) % 24)
@@ -27,8 +24,9 @@ const ReviewKeyExpirationCard = () => {
         display: 'flex',
         px: '30px',
         py: 6,
-        flexDir: 'row',
+        flexDir: { base: 'row', mdDown: 'column' },
         alignItems: 'center',
+        gap: { mdDown: 4 },
         justifyContent: 'space-between',
         rounded: 20,
         border: '1px solid {colors.lightGray.1}',
@@ -53,19 +51,20 @@ const ReviewKeyExpirationCard = () => {
           })}
         />
         <div className={css({ display: 'flex', flexDir: 'column', gap: 2.5 })}>
-          <h3 className={css({ color: 'black', fontSize: 24, fontWeight: 700, lineHeight: 1.2 })}>
+          <h3 className={css({ color: 'black', fontSize: { base: 24, mdDown: 14 }, fontWeight: 700, lineHeight: 1.2 })}>
             Course review reading Key
           </h3>
           <p
             className={css({
-              fontSize: 20,
+              fontSize: { base: 20, mdDown: 13 },
               color: 'darkGray.1',
               fontWeight: 500,
               lineHeight: 1.2,
               letterSpacing: '-0.4px',
             })}
           >
-            - {dateFormatter({ date: expireDate })}
+            - {dateFormatter({ date: expireDate })} {String(expireDate.getHours()).padStart(2, '0')}:
+            {String(expireDate.getMinutes()).padStart(2, '0')}
           </p>
         </div>
       </div>
@@ -73,15 +72,20 @@ const ReviewKeyExpirationCard = () => {
         <History />
         <div
           className={css({
-            fontSize: 24,
+            fontSize: { base: 24, mdDown: 14 },
             fontWeight: 500,
             lineHeight: 1.2,
             '& span': {
+              display: 'inline-flex',
               bgColor: 'white',
               border: '0.5px solid {colors.darkGray.2}',
               rounded: 4,
-              w: '46px',
-              h: '33px',
+              w: { base: '46px', mdDown: '28px' },
+              h: { base: '33px', mdDown: '20px' },
+              alignItems: 'center',
+              justifyContent: 'center',
+              fontSize: { base: 28, mdDown: 16 },
+              fontWeight: 600,
             },
           })}
         >
