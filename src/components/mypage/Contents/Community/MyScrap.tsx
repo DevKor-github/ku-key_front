6b387f3f5c7e267a9c8b8da95143be9f19c9cf0b
halionaz <samhsj@naver.com>
2024-09-15 16:34:13 +0900

    mod:: MyPage 게시글 모아보기에서 사진 안 보이도록 수정

--- src/components/mypage/Contents/Community/MyScrap.tsx
+++ src/components/mypage/Contents/Community/MyScrap.tsx
@@ -13,18 +13,29 @@ const MyScrap = () => {
   })
 
   return (
-    <div className={css({ display: 'flex', flexDir: 'column', gap: '50px', mb: 25, w: { base: 610, mdDown: 320 } })}>
-      {posts?.map(post => (
-        <PostPreview
-          key={post.id}
-          id={post.id}
-          title={post.title}
-          boardName={post.boardName}
-          user={post.user}
-          createdAt={post.createdAt}
-          content={post.content}
-          thumbnailDir={post.thumbnailDir}
-        />
+    <div className={css({ display: 'flex', flexDir: 'column', gap: '30px', mb: 25, w: { base: 610, mdDown: 320 } })}>
+      {posts?.map((post, index) => (
+        <>
+          {index !== 0 && (
+            <div
+              className={css({
+                w: 'full',
+                h: 0.25,
+                bgColor: 'lightGray.1',
+              })}
+            />
+          )}
+          <PostPreview
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            boardName={post.boardName}
+            user={post.user}
+            createdAt={post.createdAt}
+            content={post.content}
+            thumbnailDir={null}
+          />
+        </>
       ))}
       <div ref={fetchNextRef} />
     </div>
