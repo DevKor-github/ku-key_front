e93d6a9ad9d8e47c186eb1ab539673773a4e70b3
halionaz <samhsj@naver.com>
2024-09-20 21:33:45 +0900

    fix:: Panda CSS Config 경로 수정

--- panda.config.ts
+++ panda.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   preflight: true,
 
   // Where to look for your css declarations
-  include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
+  include: [`${__dirname}/src/**/*.{js,jsx,ts,tsx}`, `${__dirname}/pages/**/*.{js,jsx,ts,tsx}`],
 
   // Files to exclude
   exclude: [],
