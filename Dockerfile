b1951e7f102b01aa019893f0c998e9db82bf0153
halionaz <samhsj@naver.com>
2024-09-20 21:22:12 +0900

    Revert "chore:: vite config에 절대경로 명시"
    This reverts commit 75ee8d997b64fa527048d494950e6d9cfc8a6d3b.

--- Dockerfile
+++ Dockerfile
@@ -7,14 +7,15 @@ WORKDIR /app
 COPY package.json yarn.lock ./
 
 # KU-key fe에 필요한 패키지 설치
-RUN yarn --production
+RUN yarn install
 # serve 설치
-RUN yarn global add serve
+# RUN yarn global add serve
 # 현재 디렉토리의 모든 파일을 도커 컨테이너의 작업 디렉토리에 복사
 COPY . .
-RUN yarn build
+# RUN yarn build
 
-EXPOSE 3000
+EXPOSE 5173
 
 # serve
-CMD ["serve", "-s", "dist"]
+# CMD ["serve", "-s", "dist"]
+CMD ["yarn", "dev"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
-import { resolve } from 'path'
-
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: [
-      { find: '@', replacement: resolve(__dirname, 'src') },
-      {
-        find: '@styled-system',
-        replacement: resolve(__dirname, 'styled-system'),
-      },
-    ],
-  },
   plugins: [react(), tsconfigPaths()],
 })
