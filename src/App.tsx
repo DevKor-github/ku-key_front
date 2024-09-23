544853525d196bccbe4e9bc020e45f5e227b0c41
halionaz <samhsj@naver.com>
2024-09-24 00:06:05 +0900

    mod: Amplitude init script tag에서 tsx provider로

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useRoutes } from 'react-router-dom'
 
 import routes from '@/lib/router/router'
+import AmplitudeProvider from '@/util/AmplitudeProvider'
 import AuthProvider from '@/util/auth/AuthProvider'
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,6 +20,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       <AuthProvider />
+      <AmplitudeProvider />
       {router}
     </QueryClientProvider>
   )
