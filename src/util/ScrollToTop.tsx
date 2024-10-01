7f79e12f0f8c48548610318cd06912534437c340
halionaz <samhsj@naver.com>
2024-10-01 19:28:33 +0900

    fix: timetable 라우팅 내에서는 스크롤이 초기화되지 않는 버그

--- src/util/ScrollToTop.tsx
+++ src/util/ScrollToTop.tsx
@@ -5,6 +5,14 @@ const ScrollToTop = () => {
   const location = useLocation()
   const curPath = location.pathname
   const curPathRoot = curPath.split('/')[1]
+  const curPathLeaf = curPath.split('/')[2]
+
+  useEffect(() => {
+    // timetable은 예외로 항상 스크롤 초기화
+    if (curPathRoot === 'timetable') {
+      window.scrollTo(0, 0)
+    }
+  }, [curPathLeaf, curPathRoot])
 
   useEffect(() => {
     window.scrollTo(0, 0)
