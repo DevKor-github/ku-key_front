aaa757ff0b6359794939aa112d3c2eec40b3f863
halionaz <samhsj@naver.com>
2024-10-01 22:27:28 +0900

    fix: curPath 바뀔 때 항상 Scroll Up - useScrollUp 훅 만료

--- src/util/ScrollToTop.tsx
+++ src/util/ScrollToTop.tsx
@@ -4,16 +4,10 @@ import { useLocation } from 'react-router-dom'
 const ScrollToTop = () => {
   const location = useLocation()
   const curPath = location.pathname
-  const curPathRoot = curPath.split('/')[1]
-
-  // timetable은 예외로 항상 스크롤 초기화
-  if (curPathRoot === 'timetable') {
-    window.scrollTo(0, 0)
-  }
 
   useEffect(() => {
     window.scrollTo(0, 0)
-  }, [curPathRoot])
+  }, [curPath])
 
   return <></>
 }
