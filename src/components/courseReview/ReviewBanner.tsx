37871006fc2d5ea2f813860c2fd9364d143c2894
halionaz <samhsj@naver.com>
2024-10-04 23:41:31 +0900

    fix(rwd): 강의평 Course Info Page 반응형 대응

--- src/components/courseReview/ReviewBanner.tsx
+++ src/components/courseReview/ReviewBanner.tsx
@@ -9,13 +9,13 @@ const ReviewBanner = ({ title }: ReviewBannerProps) => {
       className={css({
         h: 35,
         w: '100%',
-        pl: 64,
         display: 'flex',
         alignItems: 'center',
         color: 'black.2',
         fontWeight: 800,
-        fontSize: 32,
+        fontSize: { base: 32, mdDown: 26, xsDown: 22 },
         bgColor: 'bg.gray',
+        px: { base: 60, lgDown: 26, mdDown: 5 },
       })}
     >
       {title}
