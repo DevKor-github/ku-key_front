142f4082b2b294319782a9bf4fe50185abb8d239
SeungminCha <dominico1225@gmail.com>
2024-10-06 18:36:26 +0900

    fix: blob의 타입을 "image/jpeg" 로 지정

--- src/util/create-file-from-url.ts
+++ src/util/create-file-from-url.ts
@@ -1,5 +1,5 @@
 export const createFileFromUrl = async (url: string, filename: string) => {
   const response = await fetch(url, { mode: 'cors' })
   const blob = await response.blob()
-  return new File([blob], filename, { type: blob.type })
+  return new File([blob], filename, { type: 'image/jpg' })
 }
