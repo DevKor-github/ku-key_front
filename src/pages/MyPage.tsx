1c44e5ea4c40d1ac1f79e6fd85bc456292871b72
halionaz <samhsj@naver.com>
2024-10-11 00:43:58 +0900

    feat(rwd): MyPage 상단 Header 모바일 뷰에서 선택적으로 보이도록 구현

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -19,6 +19,10 @@ const MyPage = () => {
   const { data: myProfileData } = useGetMyProfile()
   const isMdDown = useMediaQueryByName('mdDown')
 
+  const headerVisibility =
+    !isMdDown ||
+    !(curPage === 'community' || curPage === 'course-review' || curPage === 'delete-account' || curPage === 'password')
+
   useEffect(() => {
     if (curPage === null && !isMdDown) {
       navigate(`${location.pathname}?page=my-point`, { replace: true })
@@ -34,54 +38,56 @@ const MyPage = () => {
 
   return (
     <MypageWrapper>
-      <div
-        className={css({
-          display: 'flex',
-          flexDir: 'column',
-          w: 'full',
-          alignItems: 'flex-start',
-          color: 'white',
-          bgColor: 'bg.gray',
-        })}
-      >
+      {headerVisibility && (
         <div
           className={css({
             display: 'flex',
-            px: { base: 56, lgDown: 20, mdDown: 5 },
-            h: { base: '31rem', lgDown: '25rem', mdDown: 250 },
+            flexDir: 'column',
             w: 'full',
-            alignItems: 'center',
-            zIndex: 0,
-            bgSize: 'cover',
-            bgPosition: 'center',
-            position: 'relative',
+            alignItems: 'flex-start',
+            color: 'white',
+            bgColor: 'bg.gray',
           })}
-          style={{
-            backgroundImage: `url(${import.meta.env.VITE_API_AWS_S3_BUCKET}/fe/myPageBanner.webp)`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
         >
-          <UserInfo
-            name={myProfileData.username}
-            country={myProfileData.country}
-            point={myProfileData.point}
-            languages={myProfileData.languages}
-            homeUniversity={myProfileData.homeUniversity}
-          />
-          <img
-            src={characterConfig[myProfileData.type][myProfileData.selectedLevel]}
-            alt="My Character"
+          <div
             className={css({
-              w: { base: '28.125rem', lgDown: '22.5rem', mdDown: '250px' },
-              position: 'absolute',
-              right: { base: '9.375rem', lgDown: '6rem', mdDown: 0 },
-              top: { base: 5, lgDown: 0 },
+              display: 'flex',
+              px: { base: 56, lgDown: 20, mdDown: 5 },
+              h: { base: '31rem', lgDown: '25rem', mdDown: 250 },
+              w: 'full',
+              alignItems: 'center',
+              zIndex: 0,
+              bgSize: 'cover',
+              bgPosition: 'center',
+              position: 'relative',
             })}
-          />
+            style={{
+              backgroundImage: `url(${import.meta.env.VITE_API_AWS_S3_BUCKET}/fe/myPageBanner.webp)`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          >
+            <UserInfo
+              name={myProfileData.username}
+              country={myProfileData.country}
+              point={myProfileData.point}
+              languages={myProfileData.languages}
+              homeUniversity={myProfileData.homeUniversity}
+            />
+            <img
+              src={characterConfig[myProfileData.type][myProfileData.selectedLevel]}
+              alt="My Character"
+              className={css({
+                w: { base: '28.125rem', lgDown: '22.5rem', mdDown: '250px' },
+                position: 'absolute',
+                right: { base: '9.375rem', lgDown: '6rem', mdDown: 0 },
+                top: { base: 5, lgDown: 0 },
+              })}
+            />
+          </div>
+          <DueDateCard startDay={myProfileData.startDay} endDay={myProfileData.endDay} />
         </div>
-        <DueDateCard startDay={myProfileData.startDay} endDay={myProfileData.endDay} />
-      </div>
+      )}
       <MyPageContents myProfileData={myProfileData} curPage={curPage} setPage={setPage} />
     </MypageWrapper>
   )
