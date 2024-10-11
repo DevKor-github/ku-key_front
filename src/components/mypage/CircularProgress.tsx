cdb57776dc94e72d3ffa82c3f16fd5b0dbe87008
halionaz <samhsj@naver.com>
2024-10-11 13:45:29 +0900

    feat(rwd): MyPage 공통 컴포넌트 smDown 대응

--- src/components/mypage/CircularProgress.tsx
+++ src/components/mypage/CircularProgress.tsx
@@ -19,37 +19,45 @@ const CircularProgress = ({ progress }: CircularProgressProps) => {
   const progressOffset = ((100 - progress) / 100) * circumference
 
   return (
-    <svg width={size} height={size}>
-      <circle
-        cx={center}
-        cy={center}
-        r={radius}
-        className={css({
-          stroke: 'lightGray.2',
-          strokeWidth: { base: 15, smDown: 6 },
-          fill: 'none',
-        })}
-      />
-      <circle
-        cx={center}
-        cy={center}
-        r={radius}
-        fill="none"
-        transform={`rotate(-90 ${center} ${center})`}
-        strokeDasharray={circumference}
-        strokeDashoffset={progressOffset}
-        className={css({ stroke: 'red.2', strokeWidth: { base: 15, smDown: 6 }, strokeLinecap: 'round' })}
-      />
-      <text
-        x={center}
-        y={center}
-        textAnchor="middle"
-        dominantBaseline="middle"
-        className={css({ fontSize: { base: 24, smDown: 12 }, fontWeight: 600, fill: 'red.2' })} // 텍스트 크기를 조절합니다. 원의 크기에 따라 조정할 수 있습니다.
-      >
-        {progress}%
-      </text>
-    </svg>
+    <div
+      className={css({
+        smDown: {
+          px: 4,
+        },
+      })}
+    >
+      <svg width={size} height={size}>
+        <circle
+          cx={center}
+          cy={center}
+          r={radius}
+          className={css({
+            stroke: 'lightGray.2',
+            strokeWidth: { base: 15, smDown: 7 },
+            fill: 'none',
+          })}
+        />
+        <circle
+          cx={center}
+          cy={center}
+          r={radius}
+          fill="none"
+          transform={`rotate(-90 ${center} ${center})`}
+          strokeDasharray={circumference}
+          strokeDashoffset={progressOffset}
+          className={css({ stroke: 'red.2', strokeWidth: { base: 15, smDown: 7 }, strokeLinecap: 'round' })}
+        />
+        <text
+          x={center}
+          y={center}
+          textAnchor="middle"
+          dominantBaseline="middle"
+          className={css({ fontSize: { base: 24, smDown: 12 }, fontWeight: 600, fill: 'red.2' })} // 텍스트 크기를 조절합니다. 원의 크기에 따라 조정할 수 있습니다.
+        >
+          {progress}%
+        </text>
+      </svg>
+    </div>
   )
 }
 
