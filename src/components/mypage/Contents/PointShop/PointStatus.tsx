cc148cc2e5a52587c26cb1f2a5be0e6e6278e182
halionaz <samhsj@naver.com>
2024-10-12 11:22:49 +0900

    feat(rwd): 포인트샵 캐릭터 반응형

--- src/components/mypage/Contents/PointShop/PointStatus.tsx
+++ src/components/mypage/Contents/PointShop/PointStatus.tsx
@@ -13,33 +13,65 @@ const PointStatus = ({ name, point }: PointStatusProps) => {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'space-between',
-        pt: '22px',
-        pb: '26px',
+        pt: { base: '22px', smDown: 0 },
+        pb: { base: '26px', smDown: 0 },
         color: 'black',
-        borderBottom: '1px solid {colors.lightGray.1}',
+        borderBottom: { base: '1px solid {colors.lightGray.1}', smDown: 'none' },
         position: 'relative',
+        smDown: {
+          bgColor: 'lightGray.2',
+          px: 3.5,
+          h: 15,
+          rounded: 10,
+        },
       })}
     >
-      <span className={css({ fontSize: { base: 30, mdDown: 15 }, fontWeight: 700 })}>
-        Hello, <span className={css({ fontSize: { base: 36, mdDown: 18 }, lineHeight: 1 })}>{name}</span>
+      <span className={css({ fontSize: { base: 30, mdDown: 15, smDown: 14 }, fontWeight: 700 })}>
+        Hello,{' '}
+        <span
+          className={css({
+            fontSize: { base: 36, mdDown: 18, smDown: 16 },
+            smDown: {
+              fontWeight: 400,
+            },
+            lineHeight: 1,
+          })}
+        >
+          {name}
+        </span>
       </span>
       <span
         className={css({
-          fontSize: 24,
+          fontSize: { base: 24, mdDown: 14, smDown: 12 },
           display: 'flex',
           alignItems: 'center',
-          gap: 5,
-          mr: { base: '120px', mdDown: '55px' },
+          gap: { base: 5, smDown: 1.5 },
+          mr: { base: '120px', mdDown: '55px', smDown: '2.875rem' },
         })}
       >
-        <span className={css({ fontWeight: 500, mdDown: { display: 'none' } })}>your sugar</span>
-        <span className={css({ w: '1px', h: '17px', bgColor: 'black' })} />
-        <span className={css({ fontWeight: 600, fontSize: { base: 26, mdDown: 15 } })}>{point}</span>
+        <span className={css({ fontWeight: { base: 500, smDown: 400 } })}>your sugar</span>
+        <span
+          className={css({
+            w: { base: '1px', smDown: '0.4px' },
+            h: { base: '17px', mdDown: '10px', smDown: '7.3px' },
+            bgColor: 'black',
+          })}
+        />
+        <span
+          className={css({ fontWeight: { base: 600, smDown: 700 }, fontSize: { base: 26, mdDown: 15, smDown: 12 } })}
+        >
+          {point}
+        </span>
       </span>
       <img
         src={Sugar}
         alt="sugar"
-        className={css({ position: 'absolute', right: 0, zIndex: 1, w: { base: '118px', mdDown: '50px' } })}
+        className={css({
+          position: 'absolute',
+          right: { base: 0, smDown: 3.5 },
+          zIndex: 1,
+          w: { base: '118px', mdDown: '50px', smDown: 10 },
+        })}
       />
     </div>
   )
