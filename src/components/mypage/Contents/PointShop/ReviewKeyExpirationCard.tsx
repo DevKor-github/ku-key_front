345ad0d7def1f9677707f72b526fb38de22b7b03
halionaz <samhsj@naver.com>
2024-10-13 21:19:03 +0900

    feat(rwd): Review Key Expiration Card 반응형

--- src/components/mypage/Contents/PointShop/ReviewKeyExpirationCard.tsx
+++ src/components/mypage/Contents/PointShop/ReviewKeyExpirationCard.tsx
@@ -5,10 +5,12 @@ import { useEffect, useState } from 'react'
 import { useGetKeyExpiration } from '@/api/hooks/user'
 import PASS_IMG from '@/assets/pass.png'
 import dateFormatter from '@/util/dateFormatter'
+import { useMediaQueryByName } from '@/util/useMediaQueryByName'
 
 const ReviewKeyExpirationCard = () => {
   const { data } = useGetKeyExpiration()
   const [today, setToday] = useState(new Date())
+  const isSmDown = useMediaQueryByName('smDown')
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -31,13 +33,13 @@ const ReviewKeyExpirationCard = () => {
     <div
       className={css({
         display: 'flex',
-        px: '30px',
-        py: 6,
-        flexDir: { base: 'row', mdDown: 'column' },
+        px: { base: '30px', smDown: '12.8px' },
+        py: { base: 6, smDown: 2.5 },
+        flexDir: { base: 'row', mdDown: 'column', smDown: 'row', xsDown: 'column' },
         alignItems: 'center',
-        gap: { mdDown: 4 },
+        gap: { mdDown: 4, smDown: 0, xsDown: 1.5 },
         justifyContent: 'space-between',
-        rounded: 20,
+        rounded: { base: 20, smDown: 8.5 },
         border: '1px solid {colors.lightGray.1}',
         bgColor: 'lightGray.2',
       })}
@@ -45,7 +47,7 @@ const ReviewKeyExpirationCard = () => {
       <div
         className={css({
           display: 'flex',
-          gap: 5,
+          gap: { base: 5, smDown: 2 },
           alignItems: 'center',
         })}
       >
@@ -53,23 +55,30 @@ const ReviewKeyExpirationCard = () => {
           src={PASS_IMG}
           alt="Review Key"
           className={css({
-            w: { base: 20, mdDown: 13 },
-            h: { base: 20, mdDown: 13 },
+            w: { base: 20, mdDown: 13, smDown: '34px' },
+            h: { base: 20, mdDown: 13, smDown: '34px' },
             rounded: 5,
             flexShrink: 0,
           })}
         />
-        <div className={css({ display: 'flex', flexDir: 'column', gap: 2.5 })}>
-          <h3 className={css({ color: 'black', fontSize: { base: 24, mdDown: 14 }, fontWeight: 700, lineHeight: 1.2 })}>
+        <div className={css({ display: 'flex', flexDir: 'column', gap: { base: 2.5, smDown: 1 } })}>
+          <h3
+            className={css({
+              color: 'black',
+              fontSize: { base: 24, mdDown: 14, smDown: 10.26 },
+              fontWeight: 700,
+              lineHeight: 1.2,
+            })}
+          >
             Course review reading Key
           </h3>
           <p
             className={css({
-              fontSize: { base: 20, mdDown: 13 },
+              fontSize: { base: 20, mdDown: 13, smDown: 8.55 },
               color: 'darkGray.1',
               fontWeight: 500,
               lineHeight: 1.2,
-              letterSpacing: '-0.4px',
+              letterSpacing: { base: '-0.4px', smDown: '-0.17px' },
             })}
           >
             - {dateFormatter({ date: expireDate })} {String(expireDate.getHours()).padStart(2, '0')}:
@@ -77,29 +86,37 @@ const ReviewKeyExpirationCard = () => {
           </p>
         </div>
       </div>
-      <div className={css({ display: 'flex', flexDir: 'row', alignItems: 'center', gap: 1.5, color: 'red.1' })}>
-        <History />
+      <div
+        className={css({
+          display: 'flex',
+          flexDir: 'row',
+          alignItems: 'center',
+          gap: { base: 1.5, smDown: '2.57px' },
+          color: 'red.1',
+        })}
+      >
+        <History size={isSmDown ? 12 : 24} />
         <div
           className={css({
-            fontSize: { base: 24, mdDown: 14 },
+            fontSize: { base: 24, mdDown: 14, smDown: 10.23 },
             fontWeight: 500,
             lineHeight: 1.2,
             '& span': {
               display: 'inline-flex',
               bgColor: 'white',
               border: '0.5px solid {colors.darkGray.2}',
-              rounded: 4,
-              w: { base: '46px', mdDown: '28px' },
-              h: { base: '33px', mdDown: '20px' },
+              rounded: { base: 4, smDown: '1.7px' },
+              w: { base: '46px', mdDown: '28px', smDown: '19.6px' },
+              h: { base: '33px', mdDown: '20px', smDown: '14.12px' },
               alignItems: 'center',
               justifyContent: 'center',
-              fontSize: { base: 28, mdDown: 16 },
+              fontSize: { base: 28, mdDown: 16, smDown: 12 },
               fontWeight: 600,
             },
           })}
         >
-          <span>{days}</span> Days <span>{String(hours).padStart(2, '0')}</span>H :{' '}
-          <span>{String(minutes).padStart(2, '0')}</span>M
+          <span>{days}</span> Days <span>{String(hours).padStart(2, '0')}</span> H :{' '}
+          <span>{String(minutes).padStart(2, '0')}</span> M
         </div>
       </div>
     </div>
