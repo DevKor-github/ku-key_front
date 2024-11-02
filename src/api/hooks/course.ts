5fa02f628e276124fcde5dd61b1643bfc37e67df
haionaz <samhsj@naver.com>
2024-11-03 00:03:24 +0900

    fix: API hook param 간소화

--- src/api/hooks/course.ts
+++ src/api/hooks/course.ts
@@ -12,94 +12,52 @@ import { apiInterface } from '@/util/axios/custom-axios'
 /**
  * 학수번호를 입력하여 강의를 검색합니다.
  */
-export const getByCourseCode = async ({ courseCode, cursorId, year, semester }: GetByCourseCodeRequest) => {
-  const response = await apiInterface.get<GetCourseResponse>('/course/search-course-code', {
-    params: {
-      courseCode,
-      cursorId,
-      year,
-      semester,
-    },
-  })
+export const getByCourseCode = async (params: GetByCourseCodeRequest) => {
+  const response = await apiInterface.get<GetCourseResponse>('/course/search-course-code', { params })
   return response.data
 }
 /**
  * 전공 과목명을 입력하여 강의를 검색합니다.
  */
-export const getByCourseNameInMajor = async ({
-  courseName,
-  major,
-  cursorId,
-  year,
-  semester,
-}: GetByCourseNameInMajorRequest) => {
-  const response = await apiInterface.get<GetCourseResponse>('/course/search-major-course-name', {
-    params: { courseName, major, cursorId, year, semester },
-  })
+export const getByCourseNameInMajor = async (params: GetByCourseNameInMajorRequest) => {
+  const response = await apiInterface.get<GetCourseResponse>('/course/search-major-course-name', { params })
   return response.data
 }
 /**d
  * 교양 과목명을 입력하여 강의를 검색합니다.
  */
-export const getByCourseNameInGeneral = async ({
-  courseName,
-  cursorId,
-  year,
-  semester,
-}: GetByCourseNameInGeneralRequest) => {
-  const response = await apiInterface.get<GetCourseResponse>('/course/search-general-course-name', {
-    params: { courseName, cursorId, year, semester },
-  })
+export const getByCourseNameInGeneral = async (params: GetByCourseNameInGeneralRequest) => {
+  const response = await apiInterface.get<GetCourseResponse>('/course/search-general-course-name', { params })
   return response.data
 }
 /**
  * 전공 과목 담당 교수님 성함을 입력하여 강의를 검색합니다.
  */
-export const getByProfInMajor = async ({ professorName, major, cursorId, year, semester }: GetByProfInMajorRequest) => {
-  const response = await apiInterface.get<GetCourseResponse>('/course/search-major-professor-name', {
-    params: { professorName, major, cursorId, year, semester },
-  })
+export const getByProfInMajor = async (params: GetByProfInMajorRequest) => {
+  const response = await apiInterface.get<GetCourseResponse>('/course/search-major-professor-name', { params })
   return response.data
 }
 /**
  * 전공 과목 담당 교수님 성함을 입력하여 강의를 검색합니다.
  */
-export const getByProfInGeneral = async ({ professorName, cursorId, year, semester }: GetByProfInGeneralRequest) => {
-  const response = await apiInterface.get<GetCourseResponse>('/course/search-general-professor-name', {
-    params: { professorName, cursorId, year, semester },
-  })
+export const getByProfInGeneral = async (params: GetByProfInGeneralRequest) => {
+  const response = await apiInterface.get<GetCourseResponse>('/course/search-general-professor-name', { params })
   return response.data
 }
 /**
  * 해당 단과대의 모든 학문의 기초 강의를 조회합니다.
  */
-export const getInAcademicFoundation = async ({
-  college,
-  cursorId,
-  year,
-  semester,
-}: GetInAcademicFoundationRequest) => {
-  const response = await apiInterface.get<GetCourseResponse>('/course/academic-foundation', {
-    params: { college, cursorId, year, semester },
-  })
+export const getInAcademicFoundation = async (params: GetInAcademicFoundationRequest) => {
+  const response = await apiInterface.get<GetCourseResponse>('/course/academic-foundation', { params })
   return response.data
 }
 
-export const getGeneral = async ({ cursorId, year, semester }: Omit<GetByCourseNameInGeneralRequest, 'courseName'>) => {
-  const response = await apiInterface.get<GetCourseResponse>('/course/general', {
-    params: { cursorId, year, semester },
-  })
+export const getGeneral = async (params: Omit<GetByCourseNameInGeneralRequest, 'courseName'>) => {
+  const response = await apiInterface.get<GetCourseResponse>('/course/general', { params })
   return response.data
 }
 
-export const getMajor = async ({
-  major,
-  cursorId,
-  year,
-  semester,
-}: Omit<GetByCourseNameInMajorRequest, 'courseName'>) => {
-  const response = await apiInterface.get<GetCourseResponse>('/course/major', {
-    params: { major, cursorId, year, semester },
-  })
+export const getMajor = async (params: Omit<GetByCourseNameInMajorRequest, 'courseName'>) => {
+  const response = await apiInterface.get<GetCourseResponse>('/course/major', { params })
   return response.data
 }
