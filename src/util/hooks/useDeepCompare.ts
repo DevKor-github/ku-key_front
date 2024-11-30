5771f66349bef0b91bf9eb712780783339815eeb
haionaz <samhsj@naver.com>
2024-11-30 11:41:22 +0900

    fix: isDeepEqual 로직 수정

--- src/util/hooks/useDeepCompare.ts
+++ src/util/hooks/useDeepCompare.ts
@@ -3,27 +3,36 @@
 
 import { DependencyList, EffectCallback, useCallback, useEffect, useLayoutEffect, useMemo, useRef } from 'react'
 
-const isObject = (object: object) => {
-  return object != null && typeof object === 'object'
-}
+export const isDeepEqual = <T>(obj1: T, obj2: T): boolean => {
+  if (obj1 === obj2) return true
 
-const isDeepEqual = <T extends Record<string, any>>(obj1: T, obj2: T): boolean => {
-  const keys1 = Object.keys(obj1)
-  const keys2 = Object.keys(obj2)
+  if (Array.isArray(obj1) && Array.isArray(obj2)) {
+    if (obj1.length !== obj2.length) return false
 
-  if (keys1.length != keys2.length || !isObject(obj1) || !isObject(obj2)) return false
-
-  for (const key of keys1) {
-    const value1 = obj1[key]
-    const value2 = obj2[key]
-
-    const isObjects = isObject(value1) && isObject(value2)
-
-    if ((isObjects && !isDeepEqual(value1, value2)) || (!isObjects && value1 !== value2)) {
-      return false
-    }
+    return obj1.every((elem, index) => {
+      return isDeepEqual(elem, obj2[index])
+    })
   }
-  return true
+
+  if (typeof obj1 === 'object' && typeof obj2 === 'object' && obj1 !== null && obj2 !== null) {
+    if (Array.isArray(obj1) || Array.isArray(obj2)) return false
+
+    const keys1 = Object.keys(obj1)
+    const keys2 = Object.keys(obj2)
+
+    if (keys1.length !== keys2.length || !keys1.every(key => keys2.includes(key))) return false
+
+    for (const key in obj1) {
+      const isEqual = isDeepEqual(obj1[key], obj2[key])
+      if (!isEqual) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  return false
 }
 
 const useDependenciesDeepCompareMemoize = (dependencies: React.DependencyList) => {
