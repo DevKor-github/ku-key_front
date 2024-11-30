cff3719bf26f29047282682e127727cf43b8515a
haionaz <samhsj@naver.com>
2024-11-30 15:06:08 +0900

    fix(chore): Deep Compare es-toolkit 사용

--- src/util/hooks/useDeepCompare.ts
+++ src/util/hooks/useDeepCompare.ts
@@ -1,45 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
+import { isEqual } from 'es-toolkit'
 import { DependencyList, EffectCallback, useCallback, useEffect, useLayoutEffect, useMemo, useRef } from 'react'
-
-export const isDeepEqual = <T>(obj1: T, obj2: T): boolean => {
-  if (obj1 === obj2) return true
-
-  if (Array.isArray(obj1) && Array.isArray(obj2)) {
-    if (obj1.length !== obj2.length) return false
-
-    return obj1.every((elem, index) => {
-      return isDeepEqual(elem, obj2[index])
-    })
-  }
-
-  if (typeof obj1 === 'object' && typeof obj2 === 'object' && obj1 !== null && obj2 !== null) {
-    if (Array.isArray(obj1) || Array.isArray(obj2)) return false
-
-    const keys1 = Object.keys(obj1)
-    const keys2 = Object.keys(obj2)
-
-    if (keys1.length !== keys2.length || !keys1.every(key => keys2.includes(key))) return false
-
-    for (const key in obj1) {
-      const isEqual = isDeepEqual(obj1[key], obj2[key])
-      if (!isEqual) {
-        return false
-      }
-    }
-
-    return true
-  }
-
-  return false
-}
 
 const useDependenciesDeepCompareMemoize = (dependencies: React.DependencyList) => {
   const dependenciesRef = useRef<React.DependencyList>(dependencies)
   const signal = useRef(0)
 
-  if (!isDeepEqual(dependencies, dependenciesRef.current)) {
+  if (!isEqual(dependencies, dependenciesRef.current)) {
     dependenciesRef.current = dependencies
     signal.current += 1
   }
