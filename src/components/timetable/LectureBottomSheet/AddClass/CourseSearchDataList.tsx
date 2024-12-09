7dea2e404adbc6b7062cae00e40b9fa565349dbe
haionaz <samhsj@naver.com>
2024-12-09 22:00:55 +0900

    fix: Course Search Loading Spinner Design - isFetching을 통해 검색이 fetch 단계일 때 loading spinner를 보여줍니다

---
+++ src/components/timetable/LectureBottomSheet/AddClass/CourseSearchDataList.tsx
@@ -0,0 +1,70 @@
+import { css } from '@styled-system/css'
+import { FetchNextPageOptions, InfiniteQueryObserverResult } from '@tanstack/react-query'
+import { forwardRef, useCallback } from 'react'
+
+import { usePostCourse } from '@/api/hooks/timetable'
+import SearchLectureCard from '@/components/timetable/LectureBottomSheet/AddClass/SearchLectureCard'
+import { SearchedCourse } from '@/types/course'
+import useIntersect from '@/util/hooks/useIntersect'
+
+const SearchMessageStyle = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: 'darkGray.2',
+  fontSize: 16,
+  fontWeight: 600,
+})
+
+interface Props {
+  searchData: SearchedCourse[]
+  fetchNextPage: (options?: FetchNextPageOptions) => Promise<InfiniteQueryObserverResult<SearchedCourse[], Error>>
+  hasNextPage: boolean
+  isFetching: boolean
+  timetableId: number
+  isInitial: boolean
+}
+const CourseSearchDataList = forwardRef<HTMLDivElement, Props>(
+  ({ searchData, fetchNextPage, hasNextPage, isFetching, timetableId, isInitial }, ref) => {
+    const { mutate: postCourse } = usePostCourse()
+    const addCourse = useCallback(
+      (courseId: number) => postCourse({ courseId, timetableId }),
+      [postCourse, timetableId],
+    )
+
+    const fetchNextRef = useIntersect(async (entry, observer) => {
+      observer.unobserve(entry.target)
+      if (hasNextPage && !isFetching) fetchNextPage()
+    })
+
+    if (searchData.length)
+      return (
+        <div
+          ref={ref}
+          className={css({
+            overflowY: 'auto',
+            display: 'flex',
+            flexDir: 'column',
+            gap: 5,
+            overscrollBehavior: 'contain',
+          })}
+        >
+          {searchData.map((data, index) => (
+            <SearchLectureCard key={index} data={data} addCourse={addCourse} />
+          ))}
+          <div ref={fetchNextRef} className={css({ height: 1 })} />
+        </div>
+      )
+
+    if (isInitial)
+      return (
+        <div className={SearchMessageStyle}>
+          Enter keywords to search (e.g., course name, professor name, or course number)
+        </div>
+      )
+
+    return <div className={SearchMessageStyle}>There are no classes available for exchange students.</div>
+  },
+)
+
+export default CourseSearchDataList
--- src/components/timetable/LectureBottomSheet/AddClass/index.tsx
+++ src/components/timetable/LectureBottomSheet/AddClass/index.tsx
@@ -3,26 +3,14 @@ import { useCallback, useEffect, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { toast } from 'sonner'
 
-import { usePostCourse } from '@/api/hooks/timetable'
-import Dropdown from '@/components/timetable/Dropdown'
 import ClassSelectModal from '@/components/timetable/LectureBottomSheet/AddClass/ClassSelectModal'
-import SearchLectureCard from '@/components/timetable/LectureBottomSheet/AddClass/SearchLectureCard'
-import SearchBox from '@/components/timetable/SearchBox'
+import { COURSE_CATEGORY_LIST } from '@/components/timetable/LectureBottomSheet/AddClass/constants'
+import CourseSearchDataList from '@/components/timetable/LectureBottomSheet/AddClass/CourseSearchDataList'
+import SearchArea from '@/components/timetable/LectureBottomSheet/AddClass/SearchArea'
+import { LoadingSpinner } from '@/components/ui/spinner'
 import Toast from '@/components/ui/toast'
 import { SemesterType } from '@/types/timetable'
 import { useCourseSearch } from '@/util/hooks/useCourseSearch'
-import useIntersect from '@/util/hooks/useIntersect'
-
-const categoryList = ['All Class', 'Major', 'General Studies', 'Academic Foundations'] as const
-
-const SearchMessageStyle = css({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  color: 'darkGray.2',
-  fontSize: 16,
-  fontWeight: 600,
-})
 
 interface AddClassProps {
   timetableId: number
@@ -45,20 +33,12 @@ const AddClass = ({ timetableId, year, semester }: AddClassProps) => {
     hasNextPage,
     isFetching,
   } = useCourseSearch({ year, semester })
-  const { mutate: postCourse } = usePostCourse()
-
-  const fetchNextRef = useIntersect(async (entry, observer) => {
-    observer.unobserve(entry.target)
-    if (hasNextPage && !isFetching) fetchNextPage()
-  })
 
   useEffect(() => {
     setTimeout(() => {
       scrollSectionRef.current && scrollSectionRef.current.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
     }, 100)
   }, [searchQuery])
-
-  const addCourse = useCallback((courseId: number) => postCourse({ courseId, timetableId }), [postCourse, timetableId])
 
   const handleDropdown = useCallback(
     (toIndex: number) => {
@@ -85,7 +65,7 @@ const AddClass = ({ timetableId, year, semester }: AddClassProps) => {
   const handleMajorBtn = useCallback(
     (classification: string) => {
       setIsModalOpen(false)
-      search(() => ({ keyword: '', category: categoryList[curCategory], classification }))
+      search(() => ({ keyword: '', category: COURSE_CATEGORY_LIST[curCategory], classification }))
     },
     [curCategory, search],
   )
@@ -110,75 +90,31 @@ const AddClass = ({ timetableId, year, semester }: AddClassProps) => {
   return (
     <>
       <div className={css({ display: 'flex', flexDir: 'column', h: '100%', gap: 2.5 })}>
-        <div className={css({ display: 'flex', flexDir: 'column', gap: 2.5 })}>
-          <div className={css({ display: 'flex', justifyContent: 'space-between' })}>
-            <div
-              className={css({
-                display: 'flex',
-                gap: 3.5,
-                alignItems: 'center',
-              })}
-            >
-              <Dropdown
-                dropdownList={categoryList}
-                curIndex={curCategory}
-                setCurIndex={handleDropdown}
-                canReselect={true}
-              />
-              {searchQuery.classification && (
-                <div
-                  className={css({
-                    rounded: 'full',
-                    bgColor: 'darkGray.2',
-                    px: 2.5,
-                    py: 1,
-                    fontWeight: 700,
-                    color: 'white',
-                    fontSize: 12,
-                    textOverflow: 'ellipsis',
-                    overflow: 'hidden',
-                    whiteSpace: 'nowrap',
-                    maxW: '600px',
-                  })}
-                >
-                  {searchQuery.classification}
-                </div>
-              )}
-            </div>
-          </div>
-          <SearchBox
-            onSubmit={handleSearchBoxOnSubmit}
-            resetKeys={[searchQuery.category, searchQuery.classification]}
-          />
-        </div>
-        {searchData.length ? (
-          <div
-            ref={scrollSectionRef}
-            className={css({
-              overflowY: 'auto',
-              display: 'flex',
-              flexDir: 'column',
-              gap: 5,
-              overscrollBehavior: 'contain',
-            })}
-          >
-            {searchData.map((data, index) => (
-              <SearchLectureCard key={index} data={data} addCourse={addCourse} />
-            ))}
-            <div ref={fetchNextRef} className={css({ height: 1 })} />
-          </div>
-        ) : searchQuery.category === 'All Class' && searchQuery.keyword.length === 0 ? (
-          <div className={SearchMessageStyle}>
-            Enter keywords to search (e.g., course name, professor name, or course number)
+        <SearchArea
+          curCategory={curCategory}
+          searchQuery={searchQuery}
+          handleDropdown={handleDropdown}
+          handleSearch={handleSearchBoxOnSubmit}
+        />
+        {isFetching ? (
+          <div className={css({ h: 'full', display: 'flex', justifyContent: 'center', alignItems: 'center' })}>
+            <LoadingSpinner />
           </div>
         ) : (
-          <div className={SearchMessageStyle}>There are no classes available for exchange students.</div>
+          <CourseSearchDataList
+            fetchNextPage={fetchNextPage}
+            hasNextPage={hasNextPage}
+            isFetching={isFetching}
+            searchData={searchData}
+            timetableId={timetableId}
+            isInitial={searchQuery.category === 'All Class' && searchQuery.keyword.length === 0}
+          />
         )}
       </div>
       {isModalOpen &&
         createPortal(
           <ClassSelectModal
-            category={categoryList[curCategory]}
+            category={COURSE_CATEGORY_LIST[curCategory]}
             handleMajorBtn={handleMajorBtn}
             handleQuitModal={handleQuitModal}
             curClassification={searchQuery.classification}
