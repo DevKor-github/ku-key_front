73c34ddcdd992680c523f7f8e79689778336a3fe
halion <samhsj@naver.com>
2024-12-30 11:39:39 +0900

    fix: Course Type 일정 요소 null type 추가 (#148)
    * fix: Course Type 일정 요소 null type 추가
    
    * fix(mod): 두벌이던 FriendTimetable과 TimetableLayout(개인 시간표) 코드의 중복되는 부분을 병합

--- src/components/timetable/Grid/LectureGrid.tsx
+++ src/components/timetable/Grid/LectureGrid.tsx
@@ -4,43 +4,85 @@ import { GetTimetableByTimetableIdResponse } from '@/api/types/timetable'
 import LectureSticker from '@/components/timetable/Grid/LectureSticker'
 import { TimeCell } from '@/components/timetable/Grid/TimetableLayout'
 import { COLOR_INFO } from '@/lib/constants/timetableColors'
-import { lectureDataPreprocess } from '@/util/timetableUtil'
+import { getWeeknTimeList, lectureDataPreprocess } from '@/util/timetableUtil'
 
 interface LectureGridProps {
   timetableId?: number
   timetableData: GetTimetableByTimetableIdResponse
-  weekCnt: number
-  timeCnt: number
   isMine?: boolean
 }
 
-const LectureGrid = ({ timetableId, timetableData, weekCnt, timeCnt, isMine = false }: LectureGridProps) => {
-  const lecGrid = lectureDataPreprocess(timetableData.courses, timetableData.schedules, weekCnt, timeCnt)
+const LectureGrid = ({ timetableId, timetableData, isMine = false }: LectureGridProps) => {
+  const { time, week } = getWeeknTimeList(timetableData.courses, timetableData.schedules)
+  const weekCnt = week.length
+  const timeCnt = time.length - 1
+
+  const { lectureGrid } = lectureDataPreprocess(timetableData.courses, timetableData.schedules, weekCnt, timeCnt)
   const colorTheme = timetableData.color
   let lecCnt = 0
 
   return (
-    <div className={css({ display: 'grid' })} style={{ gridTemplateColumns: `repeat(${weekCnt}, 1fr)` }}>
-      {lecGrid.map((lectures, gridInd) => {
-        return (
-          <div
-            key={gridInd}
-            className={TimeCell({ lectureGrid: true, end: gridInd === weekCnt * timeCnt - 1 ? 'rightEnd' : undefined })}
-          >
-            {lectures.map((lecture, lecInd) => {
-              return (
-                <LectureSticker
-                  key={lecInd}
-                  timetableId={timetableId}
-                  data={lecture}
-                  bgColor={COLOR_INFO[colorTheme]['rand'][lecCnt++ % COLOR_INFO[colorTheme]['rand'].length]}
-                  isMine={isMine}
-                />
-              )
-            })}
-          </div>
-        )
+    <div
+      className={css({
+        display: 'flex',
+        flexDir: 'row',
+        borderLeft: '1px solid {colors.lightGray.1}',
+        roundedBottom: 10,
+        bgColor: 'white',
       })}
+    >
+      <div className={css({ display: 'flex', flexDir: 'column' })}>
+        {time.map((val, index) => {
+          return (
+            <div
+              key={index}
+              className={TimeCell({
+                sidebar: true,
+                header: index === 0,
+                end: index === time.length - 1 ? 'leftEnd' : undefined,
+              })}
+            >
+              {val}
+            </div>
+          )
+        })}
+      </div>
+      <div className={css({ display: 'flex', flexDir: 'column', flex: 1 })}>
+        <div className={css({ display: 'flex', flexDir: 'row' })}>
+          {week.map((days, index) => {
+            return (
+              <div key={index} className={css({ flex: 1 }, TimeCell.raw({ header: true }))}>
+                {days}
+              </div>
+            )
+          })}
+        </div>
+        <div className={css({ display: 'grid' })} style={{ gridTemplateColumns: `repeat(${weekCnt}, 1fr)` }}>
+          {lectureGrid.map((lectures, gridInd) => {
+            return (
+              <div
+                key={gridInd}
+                className={TimeCell({
+                  lectureGrid: true,
+                  end: gridInd === weekCnt * timeCnt - 1 ? 'rightEnd' : undefined,
+                })}
+              >
+                {lectures.map((lecture, lecInd) => {
+                  return (
+                    <LectureSticker
+                      key={lecInd}
+                      timetableId={timetableId}
+                      data={lecture}
+                      bgColor={COLOR_INFO[colorTheme]['rand'][lecCnt++ % COLOR_INFO[colorTheme]['rand'].length]}
+                      isMine={isMine}
+                    />
+                  )
+                })}
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
