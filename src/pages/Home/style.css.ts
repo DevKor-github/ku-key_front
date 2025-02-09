83f225d56490a087790969c11b69a163d0f17c71
SeungminCha <dominico1225@gmail.com>
2025-02-09 18:39:33 +0900

    feat: 홈 화면 고려대 웹사이트 파트

--- src/pages/Home/style.css.ts
+++ src/pages/Home/style.css.ts
@@ -2,13 +2,4 @@ import { style } from '@vanilla-extract/css'
 
 import { f } from '@/style'
 
-export const Wrapper = style([
-  f.flex,
-  f.directionColumn,
-  f.alignStart,
-  f.justifyCenter,
-  f.background.white,
-  { gap: '7.5rem' },
-])
-
-// display: 'flex', flexDir: 'column', alignItems: 'center', bgColor: 'bg.gray'
+export const Wrapper = style([f.flex, f.directionColumn, f.alignCenter, f.background.white, { gap: '7.5rem' }])
