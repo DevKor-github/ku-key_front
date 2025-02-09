b6ae948d6dd7e040f543c6bc23908b320e2aec6e
halionaz <samhsj@korea.ac.kr>
2025-02-09 17:44:12 +0900

    fix: Club Card & ClubList Layout

--- src/features/Club/components/ClubList/style.css.ts
+++ src/features/Club/components/ClubList/style.css.ts
@@ -3,7 +3,7 @@ import { style } from '@vanilla-extract/css'
 import { f } from '@/style'
 import { vars } from '@/theme/theme.css'
 
-export const SearchResultWrapper = style([f.flex, f.flexColumn, { gap: 10 }])
+export const SearchResultWrapper = style([f.flex, f.flexColumn, { gap: '5rem' }])
 
 export const ClubCardWrapper = style([
   { display: 'grid', gridTemplateColumns: '1fr 1fr', rowGap: '3.12rem', columnGap: '2.5rem' },
@@ -15,7 +15,6 @@ export const ClubCardWrapper = style([
 export const KeywordForDesktop = style([
   vars.typography.desktop.titleSB,
   {
-    margin: '1.875rem 0',
     color: vars.color.darkGray1,
   },
 ])
