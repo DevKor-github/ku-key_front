09f1ea854ab62ef3c88b738229ca3654a8214c4a
SeungminCha <dominico1225@gmail.com>
2025-02-09 15:56:09 +0900

    feat: home 상단 작업

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,17 @@
 import { css } from '@styled-system/css'
 import { Suspense } from 'react'
 
-import { LoadingScreen } from '@/components/ui/spinner'
+import { LoadingScreen, LoadingSpinner } from '@/components/ui/spinner'
+import HomeBanner from '@/features/HomeBanner'
 
 const HomePage = () => {
   return (
     <Suspense fallback={<LoadingScreen />}>
-      <main className={css({ display: 'flex', flexDir: 'column', alignItems: 'center', bgColor: 'bg.gray' })}></main>
+      <main className={css({ display: 'flex', flexDir: 'column', alignItems: 'center', bgColor: 'bg.gray' })}>
+        <Suspense fallback={<LoadingSpinner />}>
+          <HomeBanner />
+        </Suspense>
+      </main>
     </Suspense>
   )
 }
