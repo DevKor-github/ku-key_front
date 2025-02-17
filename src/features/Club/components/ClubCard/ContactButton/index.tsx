94dbbe4aa24f7d8a7700e5a46aa2a9a40d2c0079
halionaz <samhsj@korea.ac.kr>
2025-02-17 21:37:13 +0900

    feat: Club Detail Page 완성

--- src/features/Club/components/ClubCard/ContactButton/index.tsx
+++ src/features/Club/components/ClubCard/ContactButton/index.tsx
@@ -1,30 +1,16 @@
-import { Facebook, Instagram, Youtube } from 'lucide-react'
-
 import * as s from './style.css'
 
-import InstagramWithGradientIcon from '@/assets/icon/InstagramWithGradientIcon'
-import { Responsive } from '@/common/Responsive'
-import { useMediaQueryByName } from '@/util/hooks/useMediaQueryByName'
+import InstagramIcon from '@/assets/icon/InstagramIcon'
+import YoutubeIcon from '@/assets/icon/YoutubeIcon'
 
 interface ContactButtonProps {
-  type: 'instagram' | 'facebook' | 'youtube'
+  type: 'instagram' | 'youtube'
   url: string
 }
 const ContactButton = ({ type, url }: ContactButtonProps) => {
-  const isMobile = useMediaQueryByName('smDown')
-
-  const ICON_SIZE = isMobile ? 25 : 16
-
   return (
     <a href={url} target="_blank" className={s.Button}>
-      {type === 'facebook' ? (
-        <Facebook size={ICON_SIZE} />
-      ) : type === 'instagram' ? (
-        <Responsive desktop={<Instagram size={ICON_SIZE} />} mobile={<InstagramWithGradientIcon />} />
-      ) : (
-        <Youtube size={ICON_SIZE} />
-      )}
-      <Responsive desktop={<span>{type}</span>} />
+      {type === 'youtube' ? <YoutubeIcon /> : <InstagramIcon />}
     </a>
   )
 }
