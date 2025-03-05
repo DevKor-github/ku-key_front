f225b649b5fe679dd09fef161b5e8b34a2ae7543
SeungminCha <dominico1225@gmail.com>
2025-03-05 09:44:14 +0900

    feat: minor 스타일 조정

--- src/pages/Community/All/style.css.ts
+++ src/pages/Community/All/style.css.ts
@@ -4,4 +4,4 @@ import { f } from '@/style'
 
 export const Wrapper = style([f.flex, f.directionColumn, f.alignCenter, { marginTop: '2.5rem', gap: '2.5rem' }])
 
-export const SelectTabWrapper = style([f.flex, f.alignStart, { width: '66.6875rem' }])
+export const SelectTabWrapper = style([f.flex, f.alignStart, { width: '70.75rem' }])
