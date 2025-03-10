474537c0c5922615857eca4f1db7a2fef2316789
Seungmin Cha <75214259+Virtuso1225@users.noreply.github.com>
2025-03-11 01:20:44 +0900

    feat: 커뮤니티의 검색하는 영역의 디자인을 반영해요. - 2 (#195)
    * feat: 검색 영역의 디자인을 반영해요.
    
    * feat: 불필요한 css를 없애요.

---
+++ src/features/Community/CommunityOutlet/index.tsx
@@ -0,0 +1,26 @@
+import * as s from './style.css'
+
+import SearchBox from '@/components/timetable/SearchBox'
+import { Button } from '@/ui/Button'
+import { Typography } from '@/ui/Typography'
+
+const CommunityOutlet = () => {
+  return (
+    <div className={s.Wrapper}>
+      <div className={s.SearchWrapper}>
+        <div className={s.Header}>
+          <div className={s.Title}>
+            <Typography typography="titleSB">View Recent Posts</Typography>
+            <Typography typography="body1M" color="darkGray1">
+              Check out our recent posts
+            </Typography>
+          </div>
+          <Button variant="red">Create post</Button>
+        </div>
+        <SearchBox initialKeyword={''} placeholder={'Search posts from entire board'} onSubmit={() => {}} />
+      </div>
+    </div>
+  )
+}
+
+export default CommunityOutlet
