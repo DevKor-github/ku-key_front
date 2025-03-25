9bc6d2894dc8c86880ef9be4ea8a335601d94f95
Seungmin Cha <75214259+Virtuso1225@users.noreply.github.com>
2025-03-25 17:43:46 +0900

    feat: 검색 기능을 게시글 api 와 연결해요. - 4 (#198)
    * feat: 검색과 api fetching을 연결해요.
    
    * feat(group): pr 리뷰 피드백

--- src/features/Community/CommunityOutlet/style.css.ts
+++ src/features/Community/CommunityOutlet/style.css.ts
@@ -5,9 +5,3 @@ import { f } from '@/style'
 export const Wrapper = style([f.flex, f.directionRow, f.alignStart, { gap: '3.75rem' }])
 
 export const LeftWrapper = style([f.flex, f.directionColumn, f.wFull, { gap: '5.375rem', maxWidth: '44.5625rem' }])
-
-export const SearchWrapper = style([f.flex, f.directionColumn, f.wFull, { gap: '1.875rem' }])
-
-export const Header = style([f.flex, f.alignCenter, f.justifyBetween, f.wFull])
-
-export const Title = style([f.flex, f.directionColumn, { gap: '0.375rem' }])
