728d99f9fc00f67481db14937e8ef5b575370c8d
halionaz <samhsj@korea.ac.kr>
2025-04-29 14:20:28 +0900

    feat: Image if there is no friend timetable

--- src/components/timetable/Grid/NullTable/index.tsx
+++ src/components/timetable/Grid/NullTable/index.tsx
@@ -1,10 +1,12 @@
 import * as s from './style.css'
 
+import NoTimetableImage from '@/assets/no-search-result.png'
 import { Typography } from '@/ui/Typography'
 
 const NullTable = () => {
   return (
     <div className={s.Wrapper}>
+      <img src={NoTimetableImage} className={s.Image} alt="Timetable hasn't been created yet!" />
       <Typography typography="heading2SB" mobileTypography="headingSB" color="darkGray1">
         Timetable hasn't been created yet!
       </Typography>
