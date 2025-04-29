ce5f53386c6b2ded5250c24d0fbe9119807ffb9c
halion <samhsj@naver.com>
2025-04-29 18:21:59 +0900

    [#224] 결과 없을 때 쿠키 캐릭터 보여주기 (#225)
    * feat: No Result Character Image
    
    * feat: Image if there is no friend timetable

--- src/components/timetable/Friend/SearchResult.tsx
+++ src/components/timetable/Friend/SearchResult.tsx
@@ -2,6 +2,7 @@ import { css } from '@styled-system/css'
 import { memo, useMemo } from 'react'
 
 import { useGetSearchUser } from '@/api/hooks/friends'
+import NoFriendImage from '@/assets/no-friend-result.png'
 import FriendCard from '@/components/timetable/Friend/FriendCard'
 import { SearchFriend } from '@/components/timetable/Friend/FriendsManage'
 import { useQueryParams } from '@/util/hooks/useQueryParams'
@@ -11,9 +12,15 @@ const SearchResult = memo(() => {
 
   const { data: searchResultData } = useGetSearchUser({ username: queryParam?.username ?? '' })
 
-  const resultText = useMemo(() => {
-    if (queryParam.username === undefined) return `Enter your friend's username to search`
-    else if (searchResultData === '') return 'No search results'
+  const resultJSX = useMemo(() => {
+    if (queryParam.username === undefined) return <>Enter your friend's username to search</>
+    if (searchResultData === '')
+      return (
+        <>
+          <img src={NoFriendImage} alt="No Search Result" className={css({ height: '5rem', opacity: 0.7 })} />
+          <p>No search results</p>
+        </>
+      )
   }, [queryParam.username, searchResultData])
 
   return (
@@ -33,11 +40,14 @@ const SearchResult = memo(() => {
           fontSize: 16,
           color: 'lightGray.1',
           display: 'flex',
+          flexDir: 'column',
+          alignItems: 'center',
           justifyContent: 'center',
+          gap: '1rem',
           pt: 5,
         })}
       >
-        {resultText}
+        {resultJSX}
       </div>
       {searchResultData && <FriendCard type="search" data={searchResultData} />}
     </div>
--- src/components/timetable/Grid/NullTable/style.css.ts
+++ src/components/timetable/Grid/NullTable/style.css.ts
@@ -7,11 +7,18 @@ export const Wrapper = style([
   f.wFull,
   f.hFull,
   f.flexCenter,
+  f.flexColumn,
   {
     height: '30rem',
     backgroundColor: vars.color.bgGray,
     borderRadius: '10px',
     border: '1px solid',
     borderColor: vars.color.lightGray1,
+    gap: '1rem',
   },
 ])
+
+export const Image = style({
+  width: '7rem',
+  opacity: 0.7,
+})
