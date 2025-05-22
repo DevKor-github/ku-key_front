5f835f8b6348dd2a666ae94b2f3fe7c0e82ba24e
Seungmin Cha <75214259+Virtuso1225@users.noreply.github.com>
2025-05-23 08:52:33 +0900

    feat: 모바일 피드 아이템 작업을 해요. (#232)

--- src/features/Community/PostAll/index.tsx
+++ src/features/Community/PostAll/index.tsx
@@ -1,9 +1,13 @@
 import * as s from './style.css'
 
 import FeedItem from '@/domain/Post/components/FeedItem'
+import MobileFeedItem from '@/domain/Post/components/MobileFeedItem'
 import { useReadCommunityPostsAll } from '@/domain/Post/hooks/useReadCommunityPostsAll'
+import { PostPreviewWithBoardName } from '@/packages/api/ku-key/models'
 import Pagination from '@/ui/Pagination'
+import { useMediaQueryByName } from '@/util/hooks/useMediaQueryByName'
 import { useQueryParams } from '@/util/hooks/useQueryParams'
+import { useCallback, useMemo } from 'react'
 
 type SearchParams = {
   keyword?: string
@@ -22,11 +26,22 @@ const CommunityPostAll = () => {
 
   if (!posts.length) return <div>No Search Result</div>
 
+  const isMobile = useMediaQueryByName('smDown')
+
+  const Render = useCallback(
+    (post: PostPreviewWithBoardName) => {
+      if (isMobile) {
+        return <MobileFeedItem {...post} showCommunityBadge />
+      }
+      return <FeedItem {...post} showCommunityBadge />
+    },
+    [isMobile],
+  )
   return (
     <Pagination
       items={posts}
       className={s.Wrapper}
-      render={post => <FeedItem {...post} showCommunityBadge />}
+      render={post => <Render {...post} />}
       hasNextPage={hasNextPage}
       isFetchingNextPage={isFetchingNextPage}
       fetchNextPage={fetchNextPage}
--- src/features/Community/PostAll/style.css.ts
+++ src/features/Community/PostAll/style.css.ts
@@ -2,4 +2,9 @@ import { style } from '@vanilla-extract/css'
 
 import { f } from '@/style'
 
-export const Wrapper = style([f.flex, f.directionColumn, { gap: '3.75rem' }])
+export const Wrapper = style([
+  f.flex,
+  f.directionColumn,
+  { gap: '3.75rem' },
+  f.smDown({ gap: '1rem', padding: '0 1rem 7.5rem 1rem', width: '100%', overflowX: 'hidden' }),
+])
